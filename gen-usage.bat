ff9f11062e26fa617eb47985704bcd0b39b5d0f8
Henk van Cann <hvancann@bcws.io>
2022-08-23 14:44:24 +0100

    gen-yml amended and debugged

---
+++ gen-usage.bat
@@ -0,0 +1,23 @@
+#!/bin/bash
+##
+# @Description: Steps to pass multiple parameters in shell script
+# Take single argument
+##
+
+function show_usage (){
+    printf "Usage: $0 [options [parameters]]\n"
+    printf "\n"
+    printf "Options:\n"
+    printf " -n|--name, Menu name is used in menu\n"
+    printf " -c|--column, Choose category column\n"
+    printf " -h|--help, Print help\n"
+
+return 0
+}
+
+if [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]];then
+    show_usage
+else
+    echo "Incorrect input provided"
+    show_usage
+fi
--- gen-yml.bat
+++ gen-yml.bat
@@ -4,89 +4,146 @@
 # Adjustments for use: Henk van Cann
 # Run this script from the root dir in repo
 # ------------------------------------------
+# Include the usage function:
+# .  /gen-usage.bat
+# ------------------------------------------
 USERNAME=$(id -un)
 DATETIME=$(date)
-SOURCE=Terms-WOT-manage.csv   # will stay in tact
-INPUT=Terms-workfile.csv      # will be overriden
-Yf=wot_sidebar.yml            # resulting yml file
+SOURCE=Terms-WOT-manage.txt   # will stay in tact
+INPUT=Terms-workfile.txt      # will be overridden
+HEADER=Header-workfile.txt    # will be overridden
+OUTYAMLFILE=wot_sidebar.yml          # resulting yml file
 
 sed '1d' "${SOURCE}" > "${INPUT}"   # remove the header from the file by creating a work file
-BASEDIR='_data/sidebars'
-nameStrLen=20
+sed -n '1p' "${SOURCE}" > "${HEADER}"   # add the header from the file by creating a work file
 
+string=$(cat "${HEADER}")
+# echo "${string//$'\n'/\\n}"
+
+length=${#string}
+
+if [  ${length} -gt 0 ]; then  # non-empty header string
+  IFS=';' read -r -a COLS <<< "${string}"  # Colums names in an array
+fi    # if length > 0
+
+
+# Some guidance of the output
+BASEDIR='_data/sidebars'   # Jekyll theme needs the yaml data in this dir
+NAMESTRLEN=20     # We need short menu item names
 
 # ------------------------------------------
-if [ $# -eq 1 ]; then
-  menuName=$1
-else
-  menuName="Overview"
+if [ $# -lt 1 ]; then
+  MENUNAME="Overview"
+  CATNAME="all"
+fi   # lt 1
+
+# Getopts handling or arguments handling
+
+while [ ! -z "$1" ]; do
+    case $1 in
+        --name|-n)
+            shift
+            echo "You entered name as: $1"
+            MENUNAME=$1
+            ;;
+        --column|-c)
+            shift
+            echo "You entered category as: $1"
+            CATNAME=$1
+            ;;
+        *)
+            show_usage
+            ;;
+    esac
+if [ $# -gt 0 ]; then
+  shift
 fi
+done    # while loop getopts
+
+# Verifying the existence of CATNAME in COLS array and we need the index
+if [[ ${CATNAME} == "all" ]]; then
+  COLUMNINDEX=999  # This means "all categories relevant"
+else    # looking for a matching column name
+  found='false'
+  for i in "${COLS[@]}"
+    do
+      if [ "$i" == "$CATNAME" ] ; then
+      COLUMNINDEX=$i     # assigns the first exactly matching column, following ignored
+      found='true'
+      fi  # found i
+    done  # for loop
+  if [ "$found" == "false"]; then 
+    echo "${CATNAME} not found in ${COLS[*]}"
+    exit 1
+  fi # found is false, CATNAME not in COLS
+fi # CATNAME == all
 
 OLDIFS="$IFS"               # $IFS is a special shell variable in Bash
 IFS=$';'
 
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-filename="./$BASEDIR/$Yf"
+FILENAME="./$BASEDIR/$OUTYAMLFILE"
 
 # Start write to resulting yaml file
-echo "# This is your automatically generated TOC using gen-yml.bat. The sidebar code loops through sections here and provides the appropriate formatting." > $filename
-echo "Generated running $(basename ${0}) located in $(dirname ${0}), by UID ${USERNAME} on ${DATETIME}."
-echo "" >> $filename
-echo "entries:" >> $filename
-echo "- title: sidebar" >> $filename
-echo "  product: Glossary KERI/ACDC" >> $filename
-echo "  version: 0.2" >> $filename
-echo "  folders:" >> $filename
-echo ""  >> $filename
-echo "  - title:" >> $filename
-echo "    output: pdf" >> $filename
-echo "    type: frontmatter" >> $filename
-echo "    folderitems:" >> $filename
-echo "    - title:" >> $filename
-echo "      url: /titlepage.html" >> $filename
-echo "      output: pdf" >> $filename
-echo "      type: frontmatter" >> $filename
-echo "    - title:" >> $filename
-echo "      url: /tocpage.html" >> $filename
-echo "      output: pdf" >> $filename
-echo "      type: frontmatter" >> $filename
-echo "" >> $filename
-echo "  - title: $menuName" >> $filename
-echo "    output: web, pdf" >> $filename
-echo "    folderitems:" >> $filename
+echo "# This script automatically generated this Terms menu YAML file , with arguments ${MENUNAME} and ${CATNAME}."  > $FILENAME 
+echo "# The sidebar code loops through sections here and provides the appropriate formatting." >> $FILENAME
+echo "# Generated running $(basename ${0}) located in $(dirname ${0}), by UID ${USERNAME} on ${DATETIME}." >> $FILENAME
+echo "" >> $FILENAME
+echo "entries:" >> $FILENAME
+echo "- title: sidebar" >> $FILENAME
+echo "  product: Glossary KERI/ACDC" >> $FILENAME
+echo "  version: 0.2" >> $FILENAME
+echo "  folders:" >> $FILENAME
+echo ""  >> $FILENAME
+echo "  - title:" >> $FILENAME
+echo "    output: pdf" >> $FILENAME
+echo "    type: frontmatter" >> $FILENAME
+echo "    folderitems:" >> $FILENAME
+echo "    - title:" >> $FILENAME
+echo "      url: /titlepage.html" >> $FILENAME
+echo "      output: pdf" >> $FILENAME
+echo "      type: frontmatter" >> $FILENAME
+echo "    - title:" >> $FILENAME
+echo "      url: /tocpage.html" >> $FILENAME
+echo "      output: pdf" >> $FILENAME
+echo "      type: frontmatter" >> $FILENAME
+echo "" >> $FILENAME
+echo "  - title: $menuName" >> $FILENAME
+echo "    output: web, pdf" >> $FILENAME
+echo "    folderitems:" >> $FILENAME
 
 while read Key ToIP_Fkey Philvid_Fkey Term text	link vidstart level Cat_PTEL Cat_IPEX Cat_OOBI Cat_CESR Cat_ACDC Cat_KERI Cat_SAID Cat_GLEIF
 do
     Term=$( echo $Term |  sed -e 's/^[[:space:]]*//' )  # remove preceding and trailing blanks
     Term=$( echo $Term | sed -e 's/[^A-Za-z0-9._-]/-/g')  # replace unwanted chars in filename
 
-    text=$( echo $text | sed -e 's/\:/\;/g')   # replace ':' with ';' in front matter 
+    text=$( echo $text | sed -e 's/\:/\;/g')   # replace ':' with ';' because in front matter ':' has a meaning and can occur only once per line.
 
     if [ $Cat_KERI -gt 0 ]; then
-      echo "" >> $filename
-        if [ ${#$Term} -gt $nameStrLen ]; then
-          nameTerm = ${$Term:0:$nameStrLen}
-          echo "    - title: $nameTerm" >> $filename
-          echo "      url: $link" >> $filename
-          echo "      output: web, pdf" >> filename
+      echo "" >> $FILENAME
+        if [ ${#$Term} -gt $NAMESTRLEN ]; then
+          nameTerm = ${$Term:0:$NAMESTRLEN}
+          echo "    - title: $nameTerm" >> $FILENAME
+          echo "      url: $link" >> $FILENAME
+          echo "      output: web, pdf" >> FILENAME
         fi  
     fi
-    echo "Key: $Key"                   # >> $filename
-    echo "ToIP-Fkey: $ToIP_Fkey"       # >> $filename
-	echo "Philvid-Fkey: $Philvid_Fkey" # >> $filename
-	echo "Term: $Term"       # >> $filename
-    echo "text: $text"       # >> $filename
-    echo "Link: $link"       # >> $filename
-	echo "Videostart: $vidstart" # >> $filename
-	echo "Level: $level"      # >> $filename
-    echo "Cat_PTEL: $Cat_PTEL" # >> $filename
-    echo "Cat_IPEX: $Cat_IPEX" # >> $filename
-    echo "Cat_OOBI: $Cat_OOBI" # >> $filename
-    echo "Cat_CESR: $Cat_CSER" # >> $filename
-    echo "Cat_ACDC: $Cat_ACDC" # >> $filename
-    echo "Cat_KERI: $Cat_KERI" # >> $filename
-    echo "Cat_SAID: $Cat_SAID" # >> $filename
-    echo "Cat_GLEIF: $Cat_GLEIF" # >> $filename
+    echo "Key: $Key"                   # >> $FILENAME
+    echo "ToIP-Fkey: $ToIP_Fkey"       # >> $FILENAME
+	echo "Philvid-Fkey: $Philvid_Fkey" # >> $FILENAME
+	echo "Term: $Term"       # >> $FILENAME
+    echo "text: $text"       # >> $FILENAME
+    echo "Link: $link"       # >> $FILENAME
+	echo "Videostart: $vidstart" # >> $FILENAME
+	echo "Level: $level"      # >> $FILENAME
+    echo "Cat_PTEL: $Cat_PTEL" # >> $FILENAME
+    echo "Cat_IPEX: $Cat_IPEX" # >> $FILENAME
+    echo "Cat_OOBI: $Cat_OOBI" # >> $FILENAME
+    echo "Cat_CESR: $Cat_CSER" # >> $FILENAME
+    echo "Cat_ACDC: $Cat_ACDC" # >> $FILENAME
+    echo "Cat_KERI: $Cat_KERI" # >> $FILENAME
+    echo "Cat_SAID: $Cat_SAID" # >> $FILENAME
+    echo "Cat_GLEIF: $Cat_GLEIF" # >> $FILENAME
   
 done < $INPUT
 IFS="$OLDIFS"   # $IFS is a special shell variable in Bash, set it back to the old value
