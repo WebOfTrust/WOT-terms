b11438f8c265cfbf6b237af653580b565456dd6c
kor <kor.dwarshuis@gmail.com>
2023-01-14 07:37:41 +0100

    Changed title, tagline, url, baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,10 +6,10 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'WOT terms',
+  tagline: 'Documentation',
+  url: 'https://dwarshuis.com',
+  baseUrl: '/test/wot-terms2/7/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
