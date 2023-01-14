c7868f6253734e28751c06d7d81ae4ef0b0e0dfa
kor <kor.dwarshuis@gmail.com>
2023-01-14 21:19:20 +0100

    Updated copyright notice

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -143,7 +143,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Web Of Trust.`,
       },
       prism: {
         theme: lightCodeTheme,
