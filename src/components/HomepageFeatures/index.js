53488d9e5c4480610ed51f25349d09aed0b2de06
kor <kor.dwarshuis@gmail.com>
2023-01-14 08:48:45 +0100

    New placeholder text

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'SSI made easy',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        You will find everything related to Self Sovereign Identity collected here. Everything.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'WOT terms at your fingertips',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Search at light speed. Find while you type.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Contribute',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Missing something? Contribute!
       </>
     ),
   },
