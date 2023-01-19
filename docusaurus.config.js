55a0f163614adc8929cacf308b9497984df85dd4
kor <kor@dwarshuis.com>
2023-01-19 16:27:53 +0100

    Algolia-specific code added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -149,6 +149,29 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: { // https://docusaurus.io/docs/search#connecting-algolia
+        // The application ID provided by Algolia
+        appId: 'process.env.ALGOLIA_APP_ID',
+
+        // Public API key: it is safe to commit it
+        apiKey: 'process.env.ALGOLIA_SEARCH_API_KEY',
+
+        indexName: 'process.env.ALGOLIA_INDEX_NAME',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: false,
+
+        //... other Algolia params
+      },
     }),
   plugins: [
     [
