05a4aaa517daaaa52cb0b613bd8ffd01fc707577
kor <kor@dwarshuis.com>
2023-02-01 15:51:30 +0100

    Double quotes to single quotes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,6 +2,7 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 // require('dotenv').config(); //TODO: move to elsewhere, does not belong in config
+// test
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
