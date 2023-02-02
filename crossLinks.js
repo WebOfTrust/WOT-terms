a755eaa98238f34bfbcea04933ba7e34b4994b4b
kor <kor@dwarshuis.com>
2023-02-02 13:01:55 +0100

    Wikipedia Winnie-the-Pooh text as example text

--- crossLinks.js
+++ crossLinks.js
@@ -1,7 +1,8 @@
 export const crossLinks = [
-  ["that allows the authenticity", "https://nu.nl"],
-  ["that", "https://volkskrant.nl"],
-  ["ACDC", "term_ACDC"],
-  ["CDC", "https://dvhn.nl"],
-  ["This", "term_APC"],
+  ['teddy bear', 'https://example.com/teddybear'],
+  ['bear', 'https://example.com/bear'],
+  ['Winnie-the-Pooh', 'https://example.com/winnie-the-pooh'],
+  ['Pooh', 'https://example.com/pooh'],
+  ['A. A. Milne', 'https://example.com/A-A-Milne'],
+  ['Milne', 'https://example.com/Milne'],
 ];
