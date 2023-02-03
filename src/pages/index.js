c031eac3d187d9ed6a027a268663b6609e986a5d
kor <kor@dwarshuis.com>
2023-02-03 08:19:56 +0100

    Image below title

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,12 +12,12 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
         <img
           className="hero__image"
           src={require('@site/static/img/stamp.png').default}
         />
 
-        <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
