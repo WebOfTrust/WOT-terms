10547c312a6bd8cb3bb8a0ec83a93aabb754b21a
kor <kor@dwarshuis.com>
2023-02-06 13:39:03 +0100

    More content for testing

--- crossLinks.js
+++ crossLinks.js
@@ -4,5 +4,18 @@ export const crossLinks = [
   ['Winnie-the-Pooh', 'https://example.com/winnie-the-pooh', 2],
   ['Pooh', 'https://example.com/pooh', 1],
   ['A. A. Milne', 'https://example.com/A-A-Milne', 4],
-  ['Milne', 'https://example.com/Milne', 2],
+  ['Paddington Bear', 'https://example.com/Paddington-Bear', 2],
+  ['Paddington', 'https://example.com/Paddington', 2],
+  [
+    'British fictional characters',
+    'https://example.com/British-fictional-characters',
+    2,
+  ],
+  ['characters', 'https://example.com/characters', 2],
+  [
+    'London Paddington station',
+    'https://example.com/London-Paddington-station',
+    2,
+  ],
+  ['Brown', 'https://example.com/Brown', 2],
 ];
