f7fda618f4de5f59959895ed9743017e06f438e4
Henk van Cann <hvancann@bcws.io>
2023-02-10 00:47:17 +0000

    Update docusaurus.config.js
    docusaurus-plugin-dotenv "safe:" back to false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -209,7 +209,7 @@ const config = {
       'docusaurus-plugin-dotenv',
       {
         path: './.env', // The path to your environment variables.
-        safe: true, // If false ignore safe-mode, if true load './.env.example', if a string load that file as the sample
+        safe: false, // If false ignore safe-mode, if true load './.env.example', if a string load that file as the sample
         systemvars: false, // Set to true if you would rather load all system variables as well (useful for CI purposes)
         silent: false, //  If true, all warnings will be suppressed
         expand: false, // Allows your variables to be "expanded" for reusability within your .env file
