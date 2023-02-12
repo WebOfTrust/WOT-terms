c223ea3dcf7df85f3e07da0ae3fa323f2f8c867a
kor <kor@dwarshuis.com>
2023-02-12 15:03:49 +0100

    Many changes

--- plugins/crossLinks.js
+++ plugins/crossLinks.js
@@ -2,8 +2,20 @@ import { crossLinks } from '../crossLinksIndex';
 import config from '@generated/docusaurus.config';
 
 const doCrossLinks = () => {
+  const domElementsContainer = document.querySelector(
+    '.markdown div.no-video-insert'
+  );
+
+  let insertVideos;
+  domElementsContainer === null
+    ? (insertVideos = true)
+    : (insertVideos = false);
+
   // const domElements = document.querySelectorAll('p');
-  const domElements = document.querySelectorAll('.markdown *:not(a)');
+  // const domElements = document.querySelectorAll(".markdown *:not(a)");
+  const domElements = document.querySelectorAll(
+    '.markdown div:not(.no-crosslinks) p'
+  );
 
   // part 1: replace keywords with (almost) unique ID's
   if (domElements.length > 0) {
@@ -30,17 +42,29 @@ const doCrossLinks = () => {
     domElements.forEach((element) => {
       crossLinks.forEach((textFragment) => {
         let innerText = element.innerHTML;
+
+        let strInnerTextNew =
+          `<a class='cross-link' href=${config.baseUrl}` +
+          textFragment.url +
+          '>' +
+          textFragment.keyword +
+          '</a>' +
+          ' <strong>(Level: ' +
+          textFragment.level +
+          ')</strong>';
+
+        if (textFragment.youtubeID !== undefined && insertVideos) {
+          strInnerTextNew += `<iframe class="video-test" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%" src="https://www.youtube.com/embed/${textFragment.youtubeID}?start=${textFragment.timeStart}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        }
+
         let innerTextNew = innerText.replace(
           textFragment.randomNumber,
-          `<a href=${config.baseUrl}` +
-            textFragment.url +
-            '>' +
-            textFragment.keyword +
-            '</a>' +
-            ' <strong>(Level: ' +
-            textFragment.level +
-            ')</strong>'
+          strInnerTextNew
         );
+
+        // textFragment.youtubeID !== undefined
+        //   ? (innerTextNew += `<iframe style="overflow:hidden;height:100%;width:100%" height="100%" width="100%" src="https://www.youtube.com/embed/${textFragment.youtubeID}?start=${textFragment.timeStart}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`)
+        //   : null;
 
         element.innerHTML = innerTextNew;
       });
