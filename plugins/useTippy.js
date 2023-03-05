5d2cc15f69ebb3009c191af5f7b1037cfc290691
kor <kor@dwarshuis.com>
2023-03-05 18:56:51 +0100

    Working version and cleaned

--- plugins/useTippy.js
+++ plugins/useTippy.js
@@ -1,58 +1,46 @@
-import config from "@generated/docusaurus.config";
-import tippy from "tippy.js";
-import "tippy.js/themes/light.css";
+import tippy from 'tippy.js';
+import 'tippy.js/themes/light.css';
 
 const useTippy = () => {
-  let links = document.querySelectorAll("a");
+  let links = document.querySelectorAll('article .markdown a');
 
   links.forEach((item) => {
-    // console.log(item);
-    // console.log(`${config.url}${config.baseUrl}${item.href}`);
+    let nextUrl = new URL(item.href, location).host;
 
-    // console.log("item.href: ", item.href);
-    // console.log("window.location.host: ", window.location.host);
-
-    let testURL = new URL(item.href, location).host;
-    // console.log("testURL: ", testURL);
-
-    if (testURL === window.location.host) {
-      // console.log("same");
-
+    // if on same domain
+    if (nextUrl === window.location.host) {
       fetch(item.href)
         .then(function (response) {
-          console.log("response: ", response);
-          // The API call was successful!
-          // console.log("response.text(): ", response.text());
           return response.text();
         })
         .then(function (html) {
           // Convert the HTML string into a document object
           var parser = new DOMParser();
-          var doc = parser.parseFromString(html, "text/html");
-          console.log("doc: ", doc);
+          var doc = parser.parseFromString(html, 'text/html');
 
-          // Get the domEl
-          var domEl = doc.querySelector("#definition");
-          console.log(domEl);
+          // Find the paragraph after heading with id="definition"
+          let domEl = doc.querySelector('#definition');
+
+          if (domEl !== null) {
+            let nextSibling = domEl.nextElementSibling;
+            tippy(item, {
+              theme: 'light',
+              allowHTML: true,
+              content: nextSibling.innerText + ' Visit link to see more.',
+            });
+          }
         })
         .catch(function (err) {
           // There was an error
-          console.warn("Something went wrong.", err);
+          console.warn('Something went wrong.', err);
         });
     }
-    tippy(item, {
-      theme: "light",
-      allowHTML: true,
-      // content: `<iframe src=${config.url}${config.baseUrl}${item.href}></iframe>`,
-      content: `<iframe src='${item.href}' srcdoc='<p></p>' ></iframe>`,
-    });
   });
 };
 
 export function onRouteDidUpdate({ location, previousLocation }) {
   // Don't execute if we are still on the same page; the lifecycle may be fired
   // because the hash changes (e.g. when navigating between headings)
-  if (location.pathname === previousLocation?.pathname) return;
-
   useTippy();
+  if (location.pathname === previousLocation?.pathname) return;
 }
