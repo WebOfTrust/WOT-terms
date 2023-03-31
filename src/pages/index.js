9e0e261e89ae1db762db09987b092e36c724217a
kor <kor@dwarshuis.com>
2023-03-31 10:32:33 +0200

    Removed default title and description meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,8 +36,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="Documenting everything around KERI, ACDC, CESR, OOBI, IPEX; what else have we got?"
     >
       <HomepageHeader />
       <main>
