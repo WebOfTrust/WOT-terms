529b4e4ca7907db255fb2844339df1201c56f641
kor <kor@dwarshuis.com>
2023-04-01 20:49:56 +0200

    Adds data-level to body tag based on json data

--- plugins/addLevel.js
+++ plugins/addLevel.js
@@ -1,23 +1,19 @@
+import overview from '@site/static/json/overview.json';
+/**
+ *  This plugin adds the level of the current page to the body tag. The info comes
+ */
+
 const addLevel = () => {
-  // fetch data from ./json/overview.json
-  fetch('/json/overview.json')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      // console.log(data[0].level);
-      // console.log(data[0].level[0].level);
-      // console.log(data[0].level[0].level[0].level);
-      // console.log(data[0].level[0].level[0].level[0].level);
-      // console.log(data[0].level[0].level[0].level[0].level[0].level);
-      // console.log(data[0].level[0].level[0].level[0].level[0].level[0].level);
-      // console.log(
-      //   data[0].level[0].level[0].level[0].level[0].level[0].level[0].level
-      // );
-      // console.log(
-      //   data[0].level[0].level[0].level[0].level[0].level[0].level[0].level[0]
-      //     .level
-      // );
-    });
+  const heading = document.querySelector('header h1').innerText;
+
+  // for every row in overview.json except the first one
+  overview.values.forEach((row, index) => {
+    if (index < 1) return;
+    if (heading === row[4]) {
+      // add row[8] to data-set of body
+      document.querySelector('body').dataset.level = row[8];
+    }
+  });
 };
 
 export function onRouteDidUpdate({ location, previousLocation }) {
