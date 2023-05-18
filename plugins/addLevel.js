12ddccd239baa92ae6b67188ff9a5af2535bf014
kor <kor@dwarshuis.com>
2023-05-18 16:45:08 +0200

    New data-attribute on <article>: data-type

--- plugins/addLevel.js
+++ plugins/addLevel.js
@@ -4,6 +4,38 @@ import overview from '@site/static/json/overview.json';
  */
 
 const addLevel = () => {
+  //TODO: make generic function for column number finder
+
+  // Find the column number of the column with the name 'Shortened version static copy'
+  const termColumnName = 'Term';
+  // loop through the first entry of the overview.json and find the column number of the column with the name 'Shortened version static copy'
+  let termColumnNumber;
+  for (let i = 0; i < overview.values[0].length; i++) {
+    if (overview.values[0][i].trim() === termColumnName) {
+      termColumnNumber = i;
+    }
+  }
+
+  // Find the column number of the column with the name 'Shortened version static copy'
+  const typeColumnName = 'Type';
+  // loop through the first entry of the overview.json and find the column number of the column with the name 'Shortened version static copy'
+  let typeColumnNumber;
+  for (let i = 0; i < overview.values[0].length; i++) {
+    if (overview.values[0][i].trim() === typeColumnName) {
+      typeColumnNumber = i;
+    }
+  }
+
+  // Find the column number of the column with the name 'Shortened version static copy'
+  const levelColumnName = 'level';
+  // loop through the first entry of the overview.json and find the column number of the column with the name 'Shortened version static copy'
+  let levelColumnNumber;
+  for (let i = 0; i < overview.values[0].length; i++) {
+    if (overview.values[0][i].trim() === levelColumnName) {
+      levelColumnNumber = i;
+    }
+  }
+
   if (document.querySelector('header h1')) {
     const heading = document.querySelector('header h1').innerText;
 
@@ -11,22 +43,39 @@ const addLevel = () => {
     overview.values.forEach((row, index) => {
       if (index < 1) return;
       if (
-        heading === row[4] &&
+        heading === row[termColumnNumber] &&
         // if the level is not empty
-        row[8] !== ''
+        row[levelColumnNumber] !== ''
       ) {
-        // add row[8] to data-set of body
-        document.querySelector('article').dataset.level = row[8];
+        // add row[levelColumnNumber] to data-set of body
+        document.querySelector('article').dataset.level =
+          row[levelColumnNumber];
         document.querySelector('article').classList.add('level');
-        document.querySelector('article').classList.add('level' + row[8]);
-        document.querySelector('article').classList.add(row[8]);
+        document
+          .querySelector('article')
+          .classList.add('level' + row[levelColumnNumber]);
+        document.querySelector('article').classList.add(row[levelColumnNumber]);
 
-        // to make it work with the Algolia DocSearch
-        let knowledgelevel = document.createElement('span');
-        knowledgelevel.classList.add('knowledgelevel');
-        knowledgelevel.classList.add('visuallyhidden');
-        knowledgelevel.innerHTML = row[8];
-        document.querySelector('article').prepend(knowledgelevel);
+        // // also add a span (originally for Algolia Docsearch scraper)
+        // let knowledgelevel = document.createElement('span');
+        // knowledgelevel.classList.add('knowledgelevel');
+        // knowledgelevel.classList.add('visuallyhidden');
+        // knowledgelevel.innerHTML = row[levelColumnNumber];
+        // document.querySelector('article').prepend(knowledgelevel);
+      }
+
+      if (
+        heading === row[termColumnNumber] &&
+        // if the type is not empty
+        row[typeColumnNumber] !== ''
+      ) {
+        // add row[typeColumnNumber] to data-set of body
+        document.querySelector('article').dataset.type = row[typeColumnNumber];
+        document.querySelector('article').classList.add('type');
+        document
+          .querySelector('article')
+          .classList.add('type' + row[typeColumnNumber]);
+        document.querySelector('article').classList.add(row[typeColumnNumber]);
       }
     });
   }
