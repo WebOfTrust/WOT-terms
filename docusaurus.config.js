4716edad99e35b848ddfdf4527ccfcc5a310ee2d
kor <kor@dwarshuis.com>
2023-06-01 16:06:51 +0200

    showLevels plugin disabled

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -325,7 +325,7 @@ const config = {
   clientModules: [
     require.resolve('./plugins/crossLinks'),
     // require.resolve('./plugins/animationHomepage'),
-    require.resolve('./plugins/showLevels'),
+    // require.resolve('./plugins/showLevels'),
     require.resolve('./plugins/insertVideo'),
     require.resolve('./plugins/insertSubtitles'),
     require.resolve('./plugins/dynamicTables'),
