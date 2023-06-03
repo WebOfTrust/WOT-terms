febe2122f772cacf673267e8248c902472ba5dad
kor <kor@dwarshuis.com>
2023-06-03 12:47:18 +0200

    Added check so it doesn't run again when clicking on same menu item

--- plugins/showGPTsummary.js
+++ plugins/showGPTsummary.js
@@ -110,6 +110,6 @@ const showGPTsummary = () => {
 export function onRouteDidUpdate({ location, previousLocation }) {
   // Don't execute if we are still on the same page; the lifecycle may be fired
   // because the hash changes (e.g. when navigating between headings)
-  // if (location.pathname === previousLocation?.pathname) return;
+  if (location.pathname === previousLocation?.pathname) return;
   showGPTsummary();
 }
