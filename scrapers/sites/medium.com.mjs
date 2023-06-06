0f03edb1684326dbbeb5a98549d35fc409d7bcde
kor <kor@dwarshuis.com>
2023-06-06 16:12:27 +0200

    Scraping mainContent to module

--- scrapers/sites/medium.com.mjs
+++ scrapers/sites/medium.com.mjs
@@ -1,6 +1,6 @@
 import createInput from '../modules/createInput.mjs';
 import importedScrape from '../modules/scrape.mjs';
-
+import extractMainContent from '../modules/extractMainContent.mjs';
 
 const config = {
     sitemap: await createInput({
@@ -17,42 +17,7 @@ console.log("config.sitemap");
 console.log(config.sitemap);
 
 async function process(page, domQueryForContent) {
-    const elements = await page.evaluate(
-        (domQueryForContent) => {
-            // Helper function to find the first heading element preceding the given element
-            function findPreviousHeadingElement(element) {
-                let previousElement = element.previousElementSibling;
-                while (previousElement) {
-                    if (isHeadingElement(previousElement)) {
-                        return previousElement;
-                    }
-                    previousElement = previousElement.previousElementSibling;
-                }
-                return null;
-            }
-
-            // Helper function to check if an element is a heading element (h1, h2, h3, etc.)
-            function isHeadingElement(element) {
-                const tagName = element.tagName.toLowerCase();
-                return (
-                    tagName.startsWith("h") && tagName.length === 2 && !isNaN(tagName.charAt(1))
-                );
-            }
-
-            const elements = Array.from(document.querySelectorAll(domQueryForContent));
-            return elements.map((el) => {
-                const headingElement = findPreviousHeadingElement(el);
-                return {
-                    text: el.textContent.trim(),
-                    tag: el.tagName.toLowerCase(),
-                    firstHeadingBeforeElement: headingElement
-                        ? headingElement.textContent.trim()
-                        : null,
-                };
-            });
-        },
-        domQueryForContent
-    );
+    const mainContent = await extractMainContent(page, domQueryForContent);
 
     let pageTitle = await page.$eval('.ch h1', (element) => {
         return element.textContent.trim()
@@ -61,7 +26,7 @@ async function process(page, domQueryForContent) {
 
 
     let all = {};
-    all.elements = elements;
+    all.elements = mainContent;
     all.pageTitle = pageTitle;
     return all;
 }
