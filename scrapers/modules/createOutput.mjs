d8197b20fba5e75de9f6437d91e12802b4f8160a
kor <kor@dwarshuis.com>
2023-06-07 22:52:46 +0200

    Refactoring

--- scrapers/modules/createOutput.mjs
+++ scrapers/modules/createOutput.mjs
@@ -1,6 +1,6 @@
 export default function createOutput(input) {
   let entries = [];
-  for (const element of input.elements) {
+  for (const element of input.mainContent) {
     // if an entry is not passed, create a default entry
     const entry = {
       siteName: input.siteName || 'No site name specified',
@@ -8,13 +8,13 @@ export default function createOutput(input) {
       content: element.text || '–',
       tag: element.tag || '–',
       timestamp: new Date().toISOString() || '–',
-      'hierarchy.lvl0': input.hierarchyLvl0 || '–',
-      'hierarchy.lvl1': input.hierarchyLvl1 || '–',
-      'hierarchy.lvl2': input.hierarchyLvl2 || '–',
-      'hierarchy.lvl3': input.hierarchyLvl3 || '–',
-      knowledgeLevel: input.knowledgeLevel || '–',
-      type: input.type || '–',
-      pageTitle: input.pageTitle || '–',
+      'hierarchy.lvl0': input.hierarchyLvl0 || '–',// same for all element from input.mainContent
+      'hierarchy.lvl1': input.hierarchyLvl1 || '–',// same for all element from input.mainContent
+      'hierarchy.lvl2': input.hierarchyLvl2 || '–',// same for all element from input.mainContent
+      'hierarchy.lvl3': input.hierarchyLvl3 || '–',// same for all element from input.mainContent
+      knowledgeLevel: input.knowledgeLevel || '–',// same for all element from input.mainContent
+      type: input.type || '–',// same for all element from input.mainContent
+      pageTitle: input.pageTitle || '–',// same for all element from input.mainContent
       firstHeadingBeforeElement: element.firstHeadingBeforeElement || '–',
     };
     entries.push(entry);
