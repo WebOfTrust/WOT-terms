dba5c034a5bb458b2e1b8a04fc604bf43d2a3da8
kor <kor@dwarshuis.com>
2023-06-07 21:48:07 +0200

    Refactoring

--- scrapers/modules/scrape.mjs
+++ scrapers/modules/scrape.mjs
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 import createOutput from './createOutput.mjs';
 import writeToFile from './writeToFile.mjs';
 
-export default async function scrape(o, processFunction) {
+export default async function scrape(config, customScrape) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     // Set a custom user agent header
@@ -12,18 +12,18 @@ export default async function scrape(o, processFunction) {
 
     // Iterate over each URL in the sitemap and create an array of entries for each URL
     // console.log('Indexing pages...');
-    for (const url of o.sitemap.urlset.url) {// for production
-        // for (const url of o.sitemap.urlset.url.slice(50, 63)) {// for testing
+    for (const url of config.sitemap.urlset.url) {// for production
+        // for (const url of config.sitemap.urlset.url.slice(50, 63)) {// for testing
         const pageUrl = url.loc[0];
         // console.log(`Indexing ${pageUrl}`);
 
         try {
             // Navigate to the page URL and process the page content using the specified function
             await page.goto(pageUrl);
-            const scraped = await processFunction(page, o.domQueryForContent, o.destinationFile);
+            const scraped = await customScrape(page, config.domQueryForContent);
 
             let output = createOutput({
-                siteName: o.siteName,
+                siteName: config.siteName,
                 pageUrl: pageUrl,
                 elements: scraped.elements,
                 hierarchyLvl0: scraped.hierarchyLevel0,
@@ -39,12 +39,12 @@ export default async function scrape(o, processFunction) {
             output.forEach((entry) => {
                 entries.push(entry);
             });
-            console.log('output: ', output);
+            // console.log('output: ', output);
 
         } catch (err) {
             console.error(`Error processing page ${pageUrl}: ${err}`);
         }
     }
-    writeToFile(entries, o.destinationFile);
+    writeToFile(entries, config.destinationFile);
     await browser.close();
 }
