19e6c954ac04364bf72e700a3cb78e23a9248857
kor <kor@dwarshuis.com>
2023-06-07 10:35:47 +0200

    If/else .env or github action

--- scrapers/import.sh
+++ scrapers/import.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
+
+if [[ -n "$GITHUB_ACTIONS" ]]; then
+  # Running in GitHub Actions
+  local_TYPESENSE_ADMIN_API_KEY="${{ secrets.TYPESENSE_ADMIN_API_KEY }}"
+  local_TYPESENSE_HOST="${{ secrets.TYPESENSE_HOST }}"
+  local_TYPESENSE_COLLECTION_NAME="${{ secrets.TYPESENSE_COLLECTION_NAME }}"
+else
+  # Running locally with .env file
+  source "$(pwd)/.env"
+  local_TYPESENSE_ADMIN_API_KEY="${TYPESENSE_ADMIN_API_KEY}"
+  local_TYPESENSE_HOST="${TYPESENSE_HOST}"
+  local_TYPESENSE_COLLECTION_NAME="${TYPESENSE_COLLECTION_NAME}"
+fi
+
+echo "local_TYPESENSE_COLLECTION_NAME:"
+echo $local_TYPESENSE_COLLECTION_NAME
+
+
 ##############
 ############## CONFIGURATION
 ##############
 
-source "$(pwd)/.env" //TODO: find a way to define root path
-
-# Name of the collection where the documents are to be imported
-collection_name="${TYPESENSE_COLLECTION_NAME}"
-
 # URL of the endpoint to empty the collection
 # explanation: the filter_by parameter is used to delete all documents that have a content field that is not empty
-urlDelete="https://${TYPESENSE_HOST}.a1.typesense.net/collections/${collection_name}/documents?filter_by=content:!=''"
+urlDelete="https://${local_TYPESENSE_HOST}.a1.typesense.net/collections/${local_TYPESENSE_COLLECTION_NAME}/documents?filter_by=content:!=''"
 
 # URL of the endpoint to import documents
-urlImport="https://${TYPESENSE_HOST}.a1.typesense.net/collections/${collection_name}/documents/import?action=create"
+urlImport="https://${local_TYPESENSE_HOST}.a1.typesense.net/collections/${local_TYPESENSE_COLLECTION_NAME}/documents/import?action=create"
 
 
 
@@ -43,7 +56,7 @@ done
 ############## EMPTY COLLECTION
 ##############
 
-curl -H "X-TYPESENSE-API-KEY: ${TYPESENSE_ADMIN_API_KEY}" -X DELETE $urlDelete
+curl -H "X-TYPESENSE-API-KEY: ${local_TYPESENSE_ADMIN_API_KEY}" -X DELETE $urlDelete
 
 
 ##############
@@ -63,7 +76,7 @@ for file in "$json_directory"/*.jsonl; do
         filename=$(basename "$file" .jsonl)
         
         # Execute the cURL command to import the document
-        curl -H "X-TYPESENSE-API-KEY: ${TYPESENSE_ADMIN_API_KEY}" \
+        curl -H "X-TYPESENSE-API-KEY: ${local_TYPESENSE_ADMIN_API_KEY}" \
               -X POST \
               -T "$file" \
               --http1.1 \
