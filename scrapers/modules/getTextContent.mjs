111dbdbe9063f05b87ed6060893d2fa774941689
kor <kor@dwarshuis.com>
2023-06-12 22:00:44 +0200

    It just didn't work

--- scrapers/modules/getTextContent.mjs
+++ scrapers/modules/getTextContent.mjs
@@ -1,8 +1,9 @@
 export default async function getTextContent(page, selector) {
-    // https://stackoverflow.com/a/76057011
     const elements = await page.$$(selector);
 
-    await Promise.all(elements.map(async (t) => {
+    const output = await Promise.all(elements.map(async (t) => {
         return await t.evaluate(x => x.textContent);
     }))
+    return output[0];
+
 }
