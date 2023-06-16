22c3c30900e1c424d32518c15e146b89aba11e2d
kor <kor@dwarshuis.com>
2023-06-16 18:16:05 +0200

    Documentation added

--- scrapers/main.sh
+++ scrapers/main.sh
@@ -3,18 +3,16 @@
 # Get the directory where the main.sh script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Create sitemaps. This can take a lot of time.
 # node "$SCRIPT_DIR/createSitemap.js" https://www.gleif.org --depth 3
 
+# Prepare logfile. This means start a new one.
 node "$SCRIPT_DIR/prepareLogFiles.mjs"
+echo "Preparing logfiles finished"
+
+# Scrape the websites. This can take a lot of time.
 node "$SCRIPT_DIR/extractData.mjs"
-
-echo "------------"
-echo "Extracted data"
-echo "------------"
-
+echo "Extracting data finished"
 
 source "$SCRIPT_DIR/import.sh"
-
-echo "------------"
-echo "Imported data"
-echo "------------"
+echo "Importing data finished"
