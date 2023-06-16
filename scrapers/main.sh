c8827eb27fb3d4749132a4997fa02e64af2547b9
kor <kor@dwarshuis.com>
2023-06-16 12:25:25 +0200

    Set to production mode

--- scrapers/main.sh
+++ scrapers/main.sh
@@ -3,7 +3,7 @@
 # Get the directory where the main.sh script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# node ./sitemaps-create/createSitemap.js https://www.gleif.org --depth 3
+# node "$SCRIPT_DIR/createSitemap.js" https://www.gleif.org --depth 3
 
 node "$SCRIPT_DIR/prepareLogFiles.mjs"
 node "$SCRIPT_DIR/extractData.mjs"
