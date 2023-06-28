a833d98af41d8f87ea8e2e884d28a823b1bedfd9
kor <kor@dwarshuis.com>
2023-06-28 15:16:02 +0200

    New: custom scraper for multiple repos

---
+++ scrapers/sites/github.mjs
@@ -0,0 +1,94 @@
+import createInput from '../modules/createInput.mjs';
+import scrape from '../modules/scrape.mjs';
+import extractMainContent from '../modules/extractMainContent.mjs';
+import getTextContent from '../modules/getTextContent.mjs';
+
+const configKeripy = {
+    sitemap: await createInput({
+        sourceType: 'localXMLsitemap',
+        sourcePath: 'scrapers/sitemaps/sitemap-github.com-WebOfTrust-keripy.xml',
+    }),
+    siteName: 'WebOfTrust-keripy',
+    source: 'WebOfTrust-keripy',
+    author: '–',
+    destinationFile: 'scrapers/output/WebOfTrust-keripy.json',
+    domQueryForContent: 'turbo-frame, article'
+}
+const configKeri = {
+    sitemap: await createInput({
+        sourceType: 'localXMLsitemap',
+        sourcePath: 'scrapers/sitemaps/sitemap-github.com-WebOfTrust-keri.xml',
+    }),
+    siteName: 'WebOfTrust-keri',
+    source: 'WebOfTrust-keri',
+    author: '–',
+    destinationFile: 'scrapers/output/WebOfTrust-keri.json',
+    domQueryForContent: 'turbo-frame, article'
+}
+const configCesride = {
+    sitemap: await createInput({
+        sourceType: 'localXMLsitemap',
+        sourcePath: 'scrapers/sitemaps/sitemap-github.com-WebOfTrust-keri.xml',
+    }),
+    siteName: 'WebOfTrust-cesride',
+    source: 'WebOfTrust-cesride',
+    author: '–',
+    destinationFile: 'scrapers/output/WebOfTrust-cesride.json',
+    domQueryForContent: 'turbo-frame, article'
+}
+// const configvLEI = {
+//     sitemap: await createInput({
+//         sourceType: 'localXMLsitemap',
+//         sourcePath: 'scrapers/sitemaps/sitemap-github.com-WebOfTrust-vLEI.xml',
+//     }),
+//     siteName: 'WebOfTrust-vLEI',
+//     source: 'WebOfTrust-vLEI',
+//     author: '–',
+//     destinationFile: 'scrapers/output/WebOfTrust-vLEI.json',
+//     domQueryForContent: 'turbo-frame, article'
+// }
+const configSignifyts = {
+    sitemap: await createInput({
+        sourceType: 'localXMLsitemap',
+        sourcePath: 'scrapers/sitemaps/sitemap-github.com-WebOfTrust-signify-ts.xml',
+    }),
+    siteName: 'WebOfTrust-signify-ts',
+    source: 'WebOfTrust-signify-ts',
+    author: '–',
+    destinationFile: 'scrapers/output/WebOfTrust-signify-ts.json',
+    domQueryForContent: 'turbo-frame, article'
+}
+const configKeria = {
+    sitemap: await createInput({
+        sourceType: 'localXMLsitemap',
+        sourcePath: 'scrapers/sitemaps/sitemap-github.com-WebOfTrust-keria.xml',
+    }),
+    siteName: 'WebOfTrust-keria',
+    source: 'WebOfTrust-keria',
+    author: '–',
+    destinationFile: 'scrapers/output/WebOfTrust-keria.json',
+    domQueryForContent: 'turbo-frame, article'
+}
+
+
+async function customScrape(page, domQueryForContent, pageUrl) {
+    const mainContent = await extractMainContent(page, domQueryForContent);
+    console.log('mainContent: ', mainContent);
+    // let pageTitle = await page.$eval('.repository-content h1', (element) => {
+    //     return element.textContent.trim()
+    // });
+    const pageTitle = await getTextContent(page, '#breadcrumb');
+
+    let all = {};
+    all.mainContent = mainContent;
+    all.pageTitle = pageTitle;
+    return all;
+}
+export default async function () {
+    scrape(configKeripy, customScrape);
+    scrape(configKeri, customScrape);
+    scrape(configCesride, customScrape);
+    // scrape(configvLEI, customScrape);
+    scrape(configSignifyts, customScrape);
+    scrape(configKeria, customScrape);
+};
