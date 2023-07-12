e7f67cffaee29f858862a9ec2c55379ea0292c1e
kor <kor@dwarshuis.com>
2023-07-12 22:07:59 +0200

    New

---
+++ clientModules/addBootstrapJS.js
@@ -0,0 +1,9 @@
+/**
+ * This file is used to load Bootstrap JS
+ * https://dev.to/vvo/how-to-solve-window-is-not-defined-errors-in-react-and-next-js-5f97
+ * npm run build will not run if Bootstrap is imported since window and document are not available in Node.js
+ */
+
+if (typeof window !== "undefined") {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js');
+}
