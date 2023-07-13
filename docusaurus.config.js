fd5c646317749716583faa711213e4cb83de27df
kor <kor@dwarshuis.com>
2023-07-13 21:33:18 +0200

    KERISSE -> KERISSE.org

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'KERISSE',
+  title: 'KERISSE.org',
   tagline:
     'KERI Suite Search Engine',
   url: paths.url,
