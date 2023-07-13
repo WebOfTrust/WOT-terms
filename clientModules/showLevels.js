53e17fa09015f34cf425df0965923770ac607ee9
kor <kor@dwarshuis.com>
2023-07-13 20:33:31 +0200

    Disabled onRouteUpdate

--- clientModules/showLevels.js
+++ clientModules/showLevels.js
@@ -187,9 +187,9 @@ export function onRouteDidUpdate({ location, previousLocation }) {
   // if (location.pathname === previousLocation?.pathname) return;
   showLevels('div, tr');
 }
-export function onRouteUpdate({ location, previousLocation }) {
-  // Don't execute if we are still on the same page; the lifecycle may be fired
-  // because the hash changes (e.g. when navigating between headings)
-  // if (location.pathname === previousLocation?.pathname) return;
-  showLevels('div, tr');
-}
+// export function onRouteUpdate({ location, previousLocation }) {
+//   // Don't execute if we are still on the same page; the lifecycle may be fired
+//   // because the hash changes (e.g. when navigating between headings)
+//   // if (location.pathname === previousLocation?.pathname) return;
+//   showLevels('div, tr');
+// }
