450e279fe9a2f0257126607db62290975d3b9e2c
kor <kor@dwarshuis.com>
2023-07-16 18:00:37 +0200

    Fix: wrong use of cheerio lib

--- multitools/fixOmnigraffleSvgOutput.js
+++ multitools/fixOmnigraffleSvgOutput.js
@@ -1,51 +1,33 @@
-// Import required modules
 const fs = require('fs');
 const path = require('path');
+const config = require('../docusaurus.config.js');
 const cheerio = require('cheerio');
-const config = require('../docusaurus.config.js'); // Import the Docusaurus configuration file
 
-// Define the directory where the SVG files are located
 const directoryPath = path.join(__dirname, "../static/img");
 
-// Define an array of search and replace rules
 const replacements = [
-    // Remove XML and DOCTYPE declarations
     {
         regex: new RegExp('<\\?xml version="1.0" encoding="UTF-8" standalone="no"\\?>\\n<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">', 'g'),
         replaceWith: ''
     },
-    // Replace xmlns:xl= with xmlns:xlink=
     {
         regex: new RegExp('xmlns:xl=', 'g'),
         replaceWith: 'xmlns:xlink='
     },
-    // Remove width and height attributes, only of the svg element itself
-    {
-        regex: new RegExp('width="\\d+(\\.\\d+)?" height="\\d+(\\.\\d+)?"'),
-        replaceWith: ''
-    },
-    // Replace xl:href= with xlink:href=
     {
         regex: new RegExp('xl:href=', 'g'),
         replaceWith: 'xlink:href='
     },
 ];
 
-// Read the directory containing the SVG files
 fs.readdir(directoryPath, (err, files) => {
     if (err) {
-        // Log error and exit if there's a problem reading the directory
         return console.log('Unable to scan directory: ' + err);
     }
 
-    // Iterate over each file in the directory
     files.forEach((file) => {
-        // Process only SVG files
         if (path.extname(file) === '.svg') {
             const filePath = path.join(directoryPath, file);
-
-            // Read the content of the SVG file
-
             fs.readFile(filePath, 'utf8', (err, data) => {
                 if (err) {
                     return console.log('Error reading file: ' + err);
@@ -54,22 +36,33 @@ fs.readdir(directoryPath, (err, files) => {
                 let result = data;
 
                 for (const rule of replacements) {
-                    // For all rules other than 'width' and 'height', apply them unconditionally
                     if (rule.regex.toString() !== '/width="\\d+(\\.\\d+)?" height="\\d+(\\.\\d+)?"/') {
                         result = result.replace(rule.regex, rule.replaceWith);
                     }
                 }
 
-                // Load the SVG XML structure into Cheerio
                 const $ = cheerio.load(result, { xmlMode: true });
 
-                // Remove 'width' and 'height' attributes from the root SVG element
                 if ($('svg').attr('viewBox')) {
                     $('svg').removeAttr('width');
                     $('svg').removeAttr('height');
                 }
 
-                // (Rest of your code for modifying <a> and <rect> elements here)
+                $('a').each(function () {
+                    const href = $(this).attr('xlink:href');
+                    if (href && !href.startsWith('http') && !href.includes(config.baseUrl)) {
+                        $(this).attr('xlink:href', config.baseUrl + href);
+                    }
+
+                    const a = $(this);
+                    const rects = a.find('rect');
+                    if (rects.length > 0) {
+                        rects.each(function () {
+                            a.before(this);
+                        });
+                        rects.remove(); // Remove the original <rect> elements from inside <a>
+                    }
+                });
 
                 result = $.html();
 
@@ -79,7 +72,6 @@ fs.readdir(directoryPath, (err, files) => {
                     }
                 });
             });
-
         }
     });
 });
