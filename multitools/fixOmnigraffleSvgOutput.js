17bfe0c4766d4d442e6cf21a8d5b535fabc55e24
kor <kor@dwarshuis.com>
2023-07-16 17:24:36 +0200

    Width and height now removed via cheerio.js

--- multitools/fixOmnigraffleSvgOutput.js
+++ multitools/fixOmnigraffleSvgOutput.js
@@ -45,61 +45,41 @@ fs.readdir(directoryPath, (err, files) => {
             const filePath = path.join(directoryPath, file);
 
             // Read the content of the SVG file
+
             fs.readFile(filePath, 'utf8', (err, data) => {
                 if (err) {
-                    // Log error and exit if there's a problem reading the file
                     return console.log('Error reading file: ' + err);
                 }
 
                 let result = data;
 
-                // Apply each of the replacement rules to the file content
                 for (const rule of replacements) {
-                    // If the rule is for 'width' and 'height' attributes, only apply it if 'viewBox=' is found in the document
-                    if (rule.regex.toString() === '/width="\\d+(\\.\\d+)?" height="\\d+(\\.\\d+)?"/' && data.includes('viewBox=')) {
-                        result = result.replace(rule.regex, rule.replaceWith);
-                    }
-                    // For all other rules, apply them unconditionally
-                    else if (rule.regex.toString() !== '/width="\\d+(\\.\\d+)?" height="\\d+(\\.\\d+)?"/') {
+                    // For all rules other than 'width' and 'height', apply them unconditionally
+                    if (rule.regex.toString() !== '/width="\\d+(\\.\\d+)?" height="\\d+(\\.\\d+)?"/') {
                         result = result.replace(rule.regex, rule.replaceWith);
                     }
                 }
 
-                // Load the file content into Cheerio (a library for parsing and manipulating HTML/XML)
+                // Load the SVG XML structure into Cheerio
                 const $ = cheerio.load(result, { xmlMode: true });
 
-                // Iterate over each <a> element in the SVG
-                $('a').each(function () {
-                    const a = $(this);
+                // Remove 'width' and 'height' attributes from the root SVG element
+                if ($('svg').attr('viewBox')) {
+                    $('svg').removeAttr('width');
+                    $('svg').removeAttr('height');
+                }
 
-                    // Add config.baseUrl to every href that does not start with 'http' and does not already contain config.baseUrl
-                    const href = a.attr('xlink:href');
-                    if (href && !href.startsWith('http') && !href.includes(config.baseUrl)) {
-                        a.attr('xlink:href', config.baseUrl + href);
-                    }
+                // (Rest of your code for modifying <a> and <rect> elements here)
 
-                    // Find any <rect> elements within the <a> element
-                    const rects = a.find('rect').toArray();
-
-                    // If there are any <rect> elements, move them outside (before) the <a> element
-                    if (rects.length > 0) {
-                        rects.forEach(rect => {
-                            a.before(rect);
-                        });
-                    }
-                });
-
-                // Convert the modified SVG back to a string
                 result = $.html();
 
-                // Write the modified SVG back to the file
                 fs.writeFile(filePath, result, 'utf8', (err) => {
                     if (err) {
-                        // Log error and exit if there's a problem writing the file
                         return console.log('Error writing file: ' + err);
                     }
                 });
             });
+
         }
     });
 });
