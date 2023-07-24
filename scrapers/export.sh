260055581ebafc1d4555783c7bf71ac034616584
kor <kor@dwarshuis.com>
2023-07-24 20:04:17 +0200

    New

---
+++ scrapers/export.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Author: Kor Dwarshuis
+# Created: 2023
+# Updated: -
+# Description: This script exports JSONL files from a Typesense collection (typesense.org).
+# Instructions:
+# In order to run this script, you need to have a .env file in the root of the project with the following variables:
+#   TYPESENSE_ADMIN_API_KEY
+#   TYPESENSE_HOST
+#   TYPESENSE_COLLECTION_NAME
+#   DOWNLOADS_DIR
+
+# Running locally with .env file
+source "$(pwd)/.env"
+local_TYPESENSE_ADMIN_API_KEY="${TYPESENSE_ADMIN_API_KEY}"
+local_TYPESENSE_HOST="${TYPESENSE_HOST}"
+local_TYPESENSE_COLLECTION_NAME="${TYPESENSE_COLLECTION_NAME}"
+local_DOWNLOADS_DIR="${DOWNLOADS_DIR}"
+
+download_path=${local_DOWNLOADS_DIR}
+timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+
+############## EXPORT JSONL FILES ##############
+curl -H "X-TYPESENSE-API-KEY: ${local_TYPESENSE_ADMIN_API_KEY}" \
+     "https://${local_TYPESENSE_HOST}.a1.typesense.net/collections/${local_TYPESENSE_COLLECTION_NAME}/documents/export" > "${download_path}${timestamp}-KERISSE-export.jsonl"
