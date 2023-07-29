fcaa8effd40feb0b8994d352671fa73999634a10
kor <kor@dwarshuis.com>
2023-07-29 15:52:55 +0200

    New

---
+++ scrapers/renameFilesToLowerCase.mjs
@@ -0,0 +1,40 @@
+/*
+  Author: Kor Dwarshuis
+  Created: 2023
+  Updated: -
+  Description: This script renames all files in a directory to lowercase. It requires the directory path as an argument. The script uses the Node.js fs module to read the directory and rename the files.
+*/
+
+const fs = require('fs');
+const path = require('path');
+
+function renameFilesToLowerCase(directoryPath) {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error(`Error reading directory: ${err}`);
+      return;
+    }
+
+    files.forEach((file) => {
+      const oldFilePath = path.join(directoryPath, file);
+      const newFilePath = path.join(directoryPath, file.toLowerCase());
+
+      fs.rename(oldFilePath, newFilePath, (renameErr) => {
+        if (renameErr) {
+          console.error(`Error renaming file: ${renameErr}`);
+        } else {
+          console.log(`${file} has been renamed to ${file.toLowerCase()}`);
+        }
+      });
+    });
+  });
+}
+
+// Usage: Provide the directory path as an argument when running the script
+const directoryPath = process.argv[2];
+
+if (!directoryPath) {
+  console.error('Please provide the directory path as an argument.');
+} else {
+  renameFilesToLowerCase(directoryPath);
+}
