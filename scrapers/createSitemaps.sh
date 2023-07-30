d63efb0a7e1b12869ef6deb036bec3cf12b952ae
kor <kor@dwarshuis.com>
2023-07-30 13:56:23 +0200

    New

---
+++ scrapers/createSitemaps.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Author: Kor Dwarshuis
+# Created: 2023
+
+
+# node scrapers/createSitemap.mjs --url https://keripy.readthedocs.io/en/latest/ --depth 5
+
+# Github repos
+node scrapers/createSitemapGithub.mjs WebOfTrust cardano-backer main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust cesride main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust cesrpy main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust gcp-ksm-shim main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust ietf-did-keri main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust kara main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust kassh main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust keep main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust keri main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust keri-swift main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust keria main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust keride main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust keriox main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust keripy main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust parside main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust saidide main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust schema main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust scir main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust shkr main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust signifi main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust signifide main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust signify-ts main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust signifypy main scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust vlei dev scrapers/sitemaps
+node scrapers/createSitemapGithub.mjs WebOfTrust ward main scrapers/sitemaps
+
+
