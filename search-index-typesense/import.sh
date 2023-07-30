968c058d29d1785103bf88a78b0a9838ebe2a45b
kor <kor@dwarshuis.com>
2023-07-30 17:59:16 +0200

    Renamed dir output-handmade to search-index-entries-manual

--- search-index-typesense/import.sh
+++ search-index-typesense/import.sh
@@ -41,8 +41,8 @@ urlDelete="https://${local_TYPESENSE_HOST}.a1.typesense.net/collections/${local_
 urlImport="https://${local_TYPESENSE_HOST}.a1.typesense.net/collections/${local_TYPESENSE_COLLECTION_NAME}/documents/import?action=create"
 
 # Handmade entries
-input_handmade_dir="$(pwd)/search-index-typesense/output-handmade"
-output_handmade_dir="$(pwd)/search-index-typesense/output-handmade"
+input_handmade_dir="$(pwd)/search-index-typesense/search-index-entries-manual"
+output_handmade_dir="$(pwd)/search-index-typesense/search-index-entries-manual"
 
 # Automated entries
 input_dir="$(pwd)/search-index-typesense/output"
@@ -55,7 +55,7 @@ log_dir="$(pwd)/search-index-typesense/logs"
 
 
 ############## COPY FROM HANDMADE DIR TO (MAIN) OUTPUT DIR ##############
-# Copy all .json files from the output-handmade directory to the output directory, so they will be converted to jsonl as well together with the automated entries
+# Copy all .json files from the search-index-entries-manual directory to the output directory, so they will be converted to jsonl as well together with the automated entries
 for file in "$output_handmade_dir"/*.json; do
     cp "$file" "$output_dir"
 done
