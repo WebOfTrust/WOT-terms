592aef8b8b02f56dd60a6c847bb4fceba84840d9
kor <kor@dwarshuis.com>
2023-08-01 12:33:44 +0200

    log -> logs

--- search-index-typesense/prepareLogFiles.sh
+++ search-index-typesense/prepareLogFiles.sh
@@ -4,16 +4,16 @@
 dir_path="search-index-typesense"
 
 # Check if a directory named "log" exists inside the path
-if [ -d "${dir_path}/log" ]; then
+if [ -d "${dir_path}/logs" ]; then
   # If the directory exists, delete it and everything inside
-  rm -rf "${dir_path}/log"
+  rm -rf "${dir_path}/logs"
 fi
 
-# Create a new directory named "log"
-mkdir "${dir_path}/log"
+# Create a new directory named "logs"
+mkdir "${dir_path}/logs"
 
 # Go inside the new "log" directory
-cd "${dir_path}/log"
+cd "${dir_path}/logs"
 
 # Create files with the specified names
 touch error.log import-into-search-index.log scraped.log success.log
