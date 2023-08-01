95fb876a65bab467a4ba7854a45fa0698c52b7b2
kor <kor@dwarshuis.com>
2023-08-01 16:22:20 +0200

    Added a Y/N option at beginning

--- search-index-typesense/main.sh
+++ search-index-typesense/main.sh
@@ -1,6 +1,64 @@
 #!/bin/bash
 
-# Start the scraping process.
+# Function to handle the user's choice
+function handle_choice() {
+    if [[ "$choice" == "Y" || "$choice" == "y" ]]; then
+        echo " "
+        echo " "
+        echo "************************************"
+        echo "The script will now start."
+        echo "************************************"
+        echo " "
+        echo " "
+        printf "."
+        sleep 1  # Pause for 3 seconds
+        printf  "."
+        sleep 1  # Pause for 3 seconds
+        printf "."
+        sleep 1  # Pause for 3 seconds
+        printf  "."
+        sleep 1  # Pause for 3 seconds
+        printf  "."
+        sleep 1  # Pause for 3 seconds
+
+
+        do_main
+    else
+        echo " "
+        echo " "
+        echo "************************************"
+        echo "Goodbye! You chose to exit."
+        echo "************************************"
+        echo " "
+        echo " "
+    fi
+}
+
+# Function to display the introduction text
+function display_intro() {
+    echo " "
+    echo " "
+    echo "************************************"
+    echo " "
+    echo "This script collects content from various sites and imports it into Typesense search engine."
+    echo "The whole process takes about 30 minutes (rough estimation)."
+    echo "Press 'Y' to continue or 'N' to exit."
+    echo " "
+    echo "************************************"
+    echo " "
+    echo " "
+
+}
+
+# Function to prompt the user for input
+function prompt_input() {
+    read -n 1 -r -p "Do you want to continue (Y/N)? " choice
+    echo
+}
+
+# Function to perform "deurdonderen"
+function do_main() {
+    # Start the scraping process.
 # IMPORTANT: This assumes that sitemaps are already generated. If not, create additional sitemaps first.
 # Run this script from project root: bash search-index-typesense/main.sh  
 
@@ -50,3 +108,11 @@ echo "Finding ids to urls finished" | tee -a search-index-typesense/logs/succes.
 # Import overrides into Typesense.
 source "$SCRIPT_DIR/overrides.sh"
 echo "Importing overrides finished" | tee -a search-index-typesense/logs/succes.log
+}
+
+# Main script starts here
+display_intro
+prompt_input
+handle_choice
+
+# End of script
