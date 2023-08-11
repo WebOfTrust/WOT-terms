92a40dd9d2901a26ecff67b8c4091b2fe2139bbb
kor <kor@dwarshuis.com>
2023-08-11 17:21:16 +0200

    Part that creates backup has been removed

--- search-index-typesense/modules/writeToFile.mjs
+++ search-index-typesense/modules/writeToFile.mjs
@@ -5,27 +5,11 @@
   Description: Write the json output to a file.
 */
 
-
 import fs from 'fs';
 import path from 'path';
 
 export default function writeToFile(entries, strOutput) {
   const output = path.resolve(strOutput);
-  // Check if the file already exists
-  if (fs.existsSync(output)) {
-    // Remove existing backup file (if exists)
-    const backupFile = `${output}.backup`;
-    if (fs.existsSync(backupFile)) {
-      fs.unlinkSync(backupFile);
-      console.log(`Existing backup file removed: ${backupFile}`);
-    }
-
-    // Create a new backup file with a timestamp
-    const timestamp = Date.now();
-    const newBackupFile = `${output}.backup-${timestamp}`;
-    fs.copyFileSync(output, newBackupFile);
-    console.log(`New backup file created: ${newBackupFile}`);
-  }
 
   // Write the entries array to the file
   console.log(`Writing search index to file...`);
