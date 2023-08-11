796b58851326f0e42b60cf19b22bb6581d67dbc8
kor <kor@dwarshuis.com>
2023-08-11 18:35:42 +0200

    Better naming of parameters and vars

--- search-index-typesense/modules/writeToFile.mjs
+++ search-index-typesense/modules/writeToFile.mjs
@@ -1,21 +1,21 @@
 /*
   Author: Kor Dwarshuis
   Created: 2023-03-16
-  Updated: -
+  Updated: 2023-08-11
   Description: Write the json output to a file.
 */
 
 import fs from 'fs';
 import path from 'path';
 
-export default function writeToFile(entries, strOutput) {
-  const output = path.resolve(strOutput);
+export default function writeToFile(entries, outputPath) {
+  const resolvedOutputPath = path.resolve(outputPath);
 
   // Write the entries array to the file
   console.log(`Writing search index to file...`);
   const fileContent = JSON.stringify(entries);
-  fs.writeFileSync(output, fileContent);
+  fs.writeFileSync(resolvedOutputPath, fileContent);
 
   console.log(`Indexed ${entries.length} pages`);
-  console.log(`Search index written to ${output}`);
+  console.log(`Search index written to ${resolvedOutputPath}`);
 }
