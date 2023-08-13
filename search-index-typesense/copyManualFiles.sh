ac6c46fb1f9bebd8496eae00c9f90c7ac7b1ba64
kor <kor@dwarshuis.com>
2023-08-13 18:48:53 +0200

    Better description

--- search-index-typesense/copyManualFiles.sh
+++ search-index-typesense/copyManualFiles.sh
@@ -3,31 +3,31 @@
 # Author: Kor Dwarshuis
 # Created: 2023
 # Updated: -
-# Description:
+# Description: Copy manually created files to the output directories of the automated scripts so they will be included in the final output directories
 
 
 ############## COPY SCRAPED CONTENT (JSON) FROM HANDMADE DIR TO AUTOMATED DIR ##############
-# Handmade entries
+# Handmade content entries
 search_index_entries_manual_dir="$(pwd)/search-index-typesense/search-index-entries-manual"
 
 # Automated entries
 search_index_entries_dir="$(pwd)/search-index-typesense/search-index-entries"
 
-# Copy all .json files from the handmade directory to the directory where the automatically created files go, so they will be converted to jsonl as well together with the automated entries
+# Copy all .json files from the handmade directory to the output directory
 for file in "$search_index_entries_manual_dir"/*.json; do
     cp "$file" "$search_index_entries_dir"
 done
 
 
 ############## COPY SITEMAP.XML FROM HANDMADE DIR TO AUTOMATED DIR ##############
-# Handmade entries
+# Handmade sitemaps
 sitemaps_manual_dir="$(pwd)/search-index-typesense/sitemaps-manual"
 
 # Automated entries
 sitemaps_dir="$(pwd)/search-index-typesense/sitemaps"
 
 
-# Copy manually created sitemap files to the directory where the automatically created files go
+# Copy manually created sitemap files to the output directory
 for file in "$sitemaps_manual_dir"/*.xml; do
     cp "$file" "$sitemaps_dir"
 done
