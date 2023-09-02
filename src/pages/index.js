238a29ea40a2ff93d74f9780f466ba665dd2577d
kor <kor@dwarshuis.com>
2023-09-02 17:52:01 +0200

    Renaming file

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro"
+            to="/docs/intro/intro"
           >
             START
           </Link>
