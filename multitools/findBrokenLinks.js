eb1ceaebf14aea0ce0ef7d70bd03fc6be7f7948e
kor <kor@dwarshuis.com>
2023-09-05 12:07:19 +0200

    Temporary code for testing

--- multitools/findBrokenLinks.js
+++ multitools/findBrokenLinks.js
@@ -17,107 +17,45 @@ console.log('Initialization...');
 
 // Configuration Section
 const config = {
-    outputFilePath: './logs/brokenLinks.txt',
+    outputFilePath: '/logs/brokenLinks.txt',
     githubRepo: 'WebOfTrust/WOT-terms',
     githubToken: process.env.GITHUB_ISSUE_AUTH_TOKEN
 };
 
-console.log('Configuration loaded.');
+const dataToWrite = "test";
 
-let brokenLinks = [];
-let linkCount = 0;  // New variable to keep track of links checked
-const maxLinks = 10;  // New variable to set maximum number of links for testing
-
-console.log('Starting link checking...');
-
-const siteChecker = new SiteChecker({}, {
-    link: (result) => {
-        if (linkCount >= maxLinks) {
-            console.log(`Reached test limit of ${maxLinks} links. Stopping.`);
-            siteChecker.pause();  // Pauses the link checking
-            return;
-        }
-
-        console.log(`Checking link: ${result.url.original} on ${result.base.original}`);
-        try {
-            // Create fully qualified URLs
-            const baseURL = 'https://weboftrust.github.io';
-            const urlObj = new URL(result.url.original, baseURL);
-            const baseObj = new URL(result.base.original, baseURL);
-
-            const isInternal = urlObj.hostname === baseObj.hostname;
-
-            if (result.broken && isInternal) {
-                const brokenInfo = {
-                    brokenLink: urlObj.href,
-                    foundOnPage: baseObj.href
-                };
-                brokenLinks.push(brokenInfo);
-                console.log(`Broken internal link found: ${urlObj.href}, Found on page: ${baseObj.href}`);
-            }
-        } catch (e) {
-            console.warn(`Skipping invalid URL: ${result.url.original}`);
-        }
-
-        linkCount++;  // Increment the count of links checked
-    },
-    end: async () => {
-        console.log('Checking done! Writing to file...');
-
-        // Get ISO8601 timestamp
-        const getISO8601Timestamp = () => {
-            const now = new Date();
-            return now.toISOString();
-        };
-
-        const timestamp = getISO8601Timestamp();
-
-        const numberOfBrokenLinks = brokenLinks.length;
-        let dataToWrite = "Created: " + timestamp + "\n\n";
-        dataToWrite += `Total Broken Links Found: ${numberOfBrokenLinks}\n\n`;
-        let totalBrokenLinks = `Total Broken Links Found: ${numberOfBrokenLinks}\n\n`;
-        dataToWrite += brokenLinks.map(linkInfo => {
-            return `Broken Link: ${linkInfo.brokenLink}, Found on Page: ${linkInfo.foundOnPage}`;
-        }).join('\n');
-
-        fs.writeFile(config.outputFilePath, dataToWrite, async (err) => {
-            if (err) {
-                console.error('Error writing to file:', err);
-            } else {
-                console.log(`Broken links and count written to ${config.outputFilePath}`);
-            }
-
-            console.log('Creating GitHub issue...');
-
-            // TODO: Create GitHub should not be inside the file write callback
-            // Create GitHub issue using Octokit
-            const issueData = {
-                title: 'Broken Links Report',
-                body: "Created: " + timestamp + "\n\n" + totalBrokenLinks + "<a href='https://github.com/WebOfTrust/WOT-terms/blob/main/logs/brokenLinks.txt'>See full list of broken links</a>.",
-            };
-
-            const octokit = new Octokit({
-                auth: config.githubToken
-            });
-
-            octokit.request('POST /repos/WebOfTrust/WOT-terms/issues', {
-                owner: 'WebOfTrust',
-                repo: 'WOT-terms',
-                title: issueData.title,
-                body: issueData.body,
-                labels: [
-                    'bug'
-                ],
-                headers: {
-                    'X-GitHub-Api-Version': '2022-11-28'
-                }
-            });
-
-            console.log('GitHub issue created.');
-        });
+fs.writeFile(config.outputFilePath, dataToWrite, async (err) => {
+    if (err) {
+        console.error('Error writing to file:', err);
+    } else {
+        console.log(`Test written to ${config.outputFilePath}`);
     }
+
+    console.log('Creating GitHub issue...');
+
+    // TODO: Create GitHub should not be inside the file write callback
+    // Create GitHub issue using Octokit
+    const issueData = {
+        title: 'Test',
+        body: "Test",
+    };
+
+    const octokit = new Octokit({
+        auth: config.githubToken
+    });
+
+    octokit.request('POST /repos/WebOfTrust/WOT-terms/issues', {
+        owner: 'WebOfTrust',
+        repo: 'WOT-terms',
+        title: issueData.title,
+        body: issueData.body,
+        labels: [
+            'bug'
+        ],
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+
+    console.log('GitHub issue created.');
 });
-
-siteChecker.enqueue('https://weboftrust.github.io/WOT-terms/');
-
-console.log('Link checking enqueued.');
