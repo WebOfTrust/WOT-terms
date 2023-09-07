70a941b06cd96eb84e7f1e8b8f9af4ccf4a264e0
kor <kor@dwarshuis.com>
2023-09-07 16:23:40 +0200

    Removed redundant code (Github Actions already takes care)

--- search-index-typesense/import.sh
+++ search-index-typesense/import.sh
@@ -10,21 +10,10 @@
 #   TYPESENSE_COLLECTION_NAME
 # External libraries: jq (https://stedolan.github.io/jq/) and curl (https://curl.se/) are required to run this script.
 
+local_TYPESENSE_ADMIN_API_KEY="${TYPESENSE_ADMIN_API_KEY}"
+local_TYPESENSE_HOST="${TYPESENSE_HOST}"
+local_TYPESENSE_COLLECTION_NAME="${TYPESENSE_COLLECTION_NAME}"
 
-# Find out if the script is running in GitHub Actions or locally
-# Via Github actions is not working yet. 
-if [[ -n "$GITHUB_ACTIONS" ]]; then
-  # Running in GitHub Actions
-  local_TYPESENSE_ADMIN_API_KEY="{{ secrets.TYPESENSE_ADMIN_API_KEY }}"
-  local_TYPESENSE_HOST="{{ secrets.TYPESENSE_HOST }}"
-  local_TYPESENSE_COLLECTION_NAME="{{ secrets.TYPESENSE_COLLECTION_NAME }}"
-else
-  # Running locally with .env file
-  source "$(pwd)/.env"
-  local_TYPESENSE_ADMIN_API_KEY="${TYPESENSE_ADMIN_API_KEY}"
-  local_TYPESENSE_HOST="${TYPESENSE_HOST}"
-  local_TYPESENSE_COLLECTION_NAME="${TYPESENSE_COLLECTION_NAME}"
-fi
 
 # Use the `local_TYPESENSE_ADMIN_API_KEY`, `local_TYPESENSE_HOST`, and `local_TYPESENSE_COLLECTION_NAME` variables anywhere in the script as needed
 
