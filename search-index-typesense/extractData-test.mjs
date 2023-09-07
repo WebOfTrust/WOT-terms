0efbf58242de7d380cbd9587eaeb5870bca06e67
kor <kor@dwarshuis.com>
2023-09-07 13:35:33 +0200

    Test script

---
+++ search-index-typesense/extractData-test.mjs
@@ -0,0 +1,33 @@
+/*
+  Author: Kor Dwarshuis
+  Created: 2023-03-16
+  Updated: -
+  Description: 
+*/
+
+
+// import scrapeWOTterms from './custom-scrapers/WOT-terms.mjs';
+// import scrapeGleif from './custom-scrapers/gleif.mjs';
+// import scrapeGleifPDF from './custom-scrapers/gleifPDF.mjs';
+// import scrapeWotgloss from './custom-scrapers/wotgloss.mjs';
+// import scrapeESSIFLab from './custom-scrapers/eSSIF-Lab.mjs';
+// import scrapeJolocom from './custom-scrapers/jolocom.io.mjs';
+// import scrapeHumancolossus from './custom-scrapers/humancolossus.mjs';
+import scrapeKsoeteman from './custom-scrapers/ksoeteman.mjs';
+// import scrapemediumCom from './custom-scrapers/medium.com.mjs';
+// import scrapeKentbull from './custom-scrapers/kentbull.com.mjs';
+// import scrapeGithub from './custom-scrapers/github.mjs';
+// import scrapeReadthedocs from './custom-scrapers/readthedocs.io.mjs';
+
+// scrapeWOTterms();
+// scrapeGleif();
+// scrapeGleifPDF();
+// scrapeWotgloss();
+// scrapeESSIFLab();
+// scrapeJolocom();
+// scrapeHumancolossus();
+scrapeKsoeteman();
+// scrapemediumCom();
+// scrapeKentbull();
+// scrapeGithub();
+// scrapeReadthedocs();
