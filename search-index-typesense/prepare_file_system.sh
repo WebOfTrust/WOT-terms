3dbafc7da404fa07f510e157c3077ca95c6537b2
kor <kor@dwarshuis.com>
2023-09-10 09:59:01 +0200

    Create a line that shows the date and time when the index file was created

--- search-index-typesense/prepare_file_system.sh
+++ search-index-typesense/prepare_file_system.sh
@@ -47,5 +47,28 @@ mkdir "${dir_path}/sitemaps"
 # Remove and recreate the index file where all the indexed websites are listed
 rm -rf "$INDEX_OVERVIEW_FILE"
 touch "$INDEX_OVERVIEW_FILE"
-echo "## Indexed in KERISSE" > "$INDEX_OVERVIEW_FILE"
+echo "# Indexed in KERISSE" > "$INDEX_OVERVIEW_FILE"
+
+# Create a line that shows the date and time when the index file was created
+# Get individual date components
+hour=$(date '+%H')
+minute=$(date '+%M')
+month=$(date '+%B')
+day=$(date '+%d')
+year=$(date '+%Y')
+
+# Determine the suffix for the day (st, nd, rd, or th)
+case "$day" in
+  1|21|31) suffix="st";;
+  2|22) suffix="nd";;
+  3|23) suffix="rd";;
+  *) suffix="th";;
+esac
+
+# Construct the complete date string
+timestamp="${hour}:${minute}, on ${month} ${day}${suffix}, ${year}"
+
+# Echo the timestamp into the file
+echo "<p id='index-created-timestamp'>Indexed at $timestamp</p>" >> "$INDEX_OVERVIEW_FILE"
+
 
