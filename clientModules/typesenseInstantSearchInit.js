03c964e275842c40689236484608cb521787fbab
kor <kor@dwarshuis.com>
2023-09-10 12:55:39 +0200

    Trying to fight an error during build (Bootstrap css declaration gets lost)

--- clientModules/typesenseInstantSearchInit.js
+++ clientModules/typesenseInstantSearchInit.js
@@ -29,10 +29,10 @@ const typesenseInstantSearchCreateDomElements = () => {
       <div class="col-md-3 p-0">
          <div class="container text-center border-bottom border-top mb-4 pb-2 pt-3">
             <div class="row">
-               <div class="col p-0">
+               <div class="col p-0" style='flex-basis: 0'><!-- Something goes wrong in build phase, this is to compensate -->
                   <h2 class="">Refine</h2>
                </div>
-               <div class="col p-0" id="clear-refinements"></div>
+               <div class="col p-0" id="clear-refinements" style='flex-basis: 0'></div>
             </div>
          </div>
          
