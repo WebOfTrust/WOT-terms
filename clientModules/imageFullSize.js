56a73eb7621781821414139715fcfa258b2e7cdb
kor <kor@dwarshuis.com>
2023-09-11 14:37:56 +0200

    Check if .markdown exists, and description added

--- clientModules/imageFullSize.js
+++ clientModules/imageFullSize.js
@@ -1,28 +1,34 @@
-/**
- * Takes div and tr that contain data-level and show/hide these via buttons
- *
- */
-
+/*
+  Author: Kor Dwarshuis
+  Created: 2023-09-11
+  Updated: -
+  Description:
+   This JavaScript file attempts to add a click event listener to the first element with the class name "markdown."
+   If such an element exists and an image within this element is clicked, the script toggles a predefined class to resize the image.
+   
+   Preconditions:
+   - A CSS class defined by the variable `imageFullSizeClass` should exist for toggling the image size.
+*/
 
 const imageFullSize = () => {
-  // const allImages = document.querySelectorAll('.markdown img');
-
   const imageFullSizeClass = 'full-page-image';
 
   // All images get a click event listener via event delegation
-  document.querySelector('.markdown').addEventListener('click', (event) => {
-    // If the clicked element is an image
-    if (event.target.tagName === 'IMG') {
-      console.log("clicked image");
+  const markdownElement = document.querySelector('.markdown');
 
-      // Toggle the full size class
-      event.target.classList.toggle(imageFullSizeClass);
-    }
-  });
+  if (markdownElement) {
+    markdownElement.addEventListener('click', (event) => {
+      // If the clicked element is an image
+      if (event.target.tagName === 'IMG') {
+        console.log("clicked image");
 
-
-
-
+        // Toggle the full size class
+        event.target.classList.toggle(imageFullSizeClass);
+      }
+    });
+  } else {
+    console.log("Element with class '.markdown' not found.");
+  }
 };
 
 // function to call when the route changes
