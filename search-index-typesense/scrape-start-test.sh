f0497423912b0c99ad6dcb669d39d38e4198912d
kor <kor@dwarshuis.com>
2023-09-13 14:53:06 +0200

    Echo replaced by log function

--- search-index-typesense/scrape-start-test.sh
+++ search-index-typesense/scrape-start-test.sh
@@ -1,3 +1,7 @@
+export INDEX_OVERVIEW_FILE="docs/02_overview/indexed-in-KERISSE.md"
+
+source ./search-index-typesense/logger.sh
+
 #########################
 # PREPARING
 #########################
@@ -7,23 +11,36 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Prepare file system.
 source "$SCRIPT_DIR/prepare_file_system.sh"
-echo "Preparing file system finished" | tee -a search-index-typesense/logs/succes.log
+# echo "Preparing file system finished" | tee -a search-index-typesense/logs/success.log
+setLogFile "success.log"
+log "Preparing file system finished"
+
 
 # Copy handmade stuff.
 source "$SCRIPT_DIR/copy_manual_files.sh"
-echo "Copying manual files finished" | tee -a search-index-typesense/logs/succes.log
+# echo "Copying manual files finished" | tee -a search-index-typesense/logs/success.log
+setLogFile "success.log"
+log "Copying manual files finished"
+
 
 # Create sitemaps.
-source "$SCRIPT_DIR/create_sitemaps.sh"
-echo "Creating sitemaps finished" | tee -a search-index-typesense/logs/succes.log
+source "$SCRIPT_DIR/create_sitemaps-test.sh"
+# echo "Creating sitemaps finished" | tee -a search-index-typesense/logs/success.log
+setLogFile "success.log"
+log "Creating sitemaps finished"
 
 # Remove unwanted urls from the sitemaps (new sitemaps generated or not)
 node "$SCRIPT_DIR/removeURLsFromSitemap.mjs"
-echo "Extracting data finished" | tee -a search-index-typesense/logs/succes.log
+# echo "Extracting data finished" | tee -a search-index-typesense/logs/success.log
+setLogFile "success.log"
+log "Extracting data finished"
+
 
 # Filenames to lowercase.
 node "$SCRIPT_DIR/renameFilesToLowerCase.mjs" search-index-typesense/sitemaps
-echo "Renaming files to lowercase finished" | tee -a search-index-typesense/logs/succes.log
+# echo "Renaming files to lowercase finished" | tee -a search-index-typesense/logs/success.log
+setLogFile "success.log"
+log "Renaming files to lowercase finished"
 
 
 
@@ -32,24 +49,34 @@ echo "Renaming files to lowercase finished" | tee -a search-index-typesense/logs
 #########################
 
 # Scrape the websites.
-node "$SCRIPT_DIR/extractData.mjs"
-echo "Extracting data finished" | tee -a search-index-typesense/logs/succes.log
+node "$SCRIPT_DIR/extractData-test.mjs"
+# echo "Extracting data finished" | tee -a search-index-typesense/logs/success.log
+setLogFile "success.log"
+log "Extracting data finished"
 
 # Split the content.jsonl file into multiple files so the size is optimal for Typesense.
 node "$SCRIPT_DIR/splitContentJSONL.mjs"
-echo "Splitting content finished" | tee -a search-index-typesense/logs/succes.log
+# echo "Splitting content finished" | tee -a search-index-typesense/logs/success.log
+setLogFile "success.log"
+log "Splitting content finished"
 
 # Sort and style the index file.
 node "$SCRIPT_DIR/sortAndStyleScrapedIndex.mjs" "$INDEX_OVERVIEW_FILE"
-echo "Sorting and styling index file finished" | tee -a search-index-typesense/logs/succes.log
+# echo "Sorting and styling index file finished" | tee -a search-index-typesense/logs/success.log
+setLogFile "success.log"
+log "Sorting and styling index file finished"
 
 # Export the data from Typesense to the downloads dir.
 source "$SCRIPT_DIR/export.sh"
-echo "Exporting data finished" | tee -a search-index-typesense/logs/succes.log
+# echo "Exporting data finished" | tee -a search-index-typesense/logs/success.log
+setLogFile "success.log"
+log "Exporting data finished"
 
 # Backup output (scrape results, handmade stuff, sitemaps etc).
 source "$SCRIPT_DIR/backup.sh"
-echo "Backup finished" | tee -a search-index-typesense/logs/succes.log
+# echo "Backup finished" | tee -a search-index-typesense/logs/success.log
+setLogFile "success.log"
+log "Backup finished"
 
 
 #########################
@@ -58,12 +85,18 @@ echo "Backup finished" | tee -a search-index-typesense/logs/succes.log
 
 # # Make collection in Typesense empty.
 # source "$SCRIPT_DIR/make_collection_empty.sh"
-# echo "Making collection empty finished" | tee -a search-index-typesense/logs/succes.log
+## echo "Making collection empty finished" | tee -a search-index-typesense/logs/success.log
+# setLogFile "success.log"
+# log "Making collection empty finished"
 
 # # Import the data into Typesense.
 # source "$SCRIPT_DIR/import.sh"
-# echo "Importing data finished" | tee -a search-index-typesense/logs/succes.log
+## echo "Importing data finished" | tee -a search-index-typesense/logs/success.log
+# setLogFile "success.log"
+# log "Importing data finished"
 
 # # Import overrides into Typesense.
 # source "$SCRIPT_DIR/overrides.sh"
-# echo "Importing overrides finished" | tee -a search-index-typesense/logs/succes.log
+## echo "Importing overrides finished" | tee -a search-index-typesense/logs/success.log
+# setLogFile "success.log"
+# log "Importing overrides finished"
