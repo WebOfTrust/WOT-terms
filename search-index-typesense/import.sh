a7dd69634c5f5bd806da2d793e77da135909d0cb
kor <kor@dwarshuis.com>
2023-09-14 13:49:26 +0200

    Sourcing .env

--- search-index-typesense/import.sh
+++ search-index-typesense/import.sh
@@ -13,6 +13,7 @@
 # Logger generates a log file with a timestamp and from which file the message comes from.
 source ./search-index-typesense/logger.sh
 
+source "$(pwd)/.env"
 local_TYPESENSE_ADMIN_API_KEY="${TYPESENSE_ADMIN_API_KEY}"
 local_TYPESENSE_HOST="${TYPESENSE_HOST}"
 local_TYPESENSE_COLLECTION_NAME="${TYPESENSE_COLLECTION_NAME}"
