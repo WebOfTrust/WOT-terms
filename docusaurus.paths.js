4870118c5c5b16472aa13a2a02eb48afbc473b2b
kor <kor@dwarshuis.com>
2023-09-19 13:44:10 +0200

    Path added to here

--- docusaurus.paths.js
+++ docusaurus.paths.js
@@ -1,6 +1,7 @@
 const paths = {
     url: "https://weboftrust.github.io",
     baseUrl: "/WOT-terms/",
+    indexedInKERISSE: "/WOT-terms/docs/overview/indexed-in-KERISSE/"
 }
 
 module.exports = paths;
