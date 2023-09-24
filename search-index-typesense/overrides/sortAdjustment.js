d80f6da45de5856bd2ec2fede2fe3405507ca7a3
kor <kor@dwarshuis.com>
2023-09-24 21:26:15 +0200

    To be used in the future

---
+++ search-index-typesense/overrides/sortAdjustment.js
@@ -0,0 +1,8 @@
+export const queriesWithSortAdjustment = [
+    // example:
+    {
+        queryString: "keri",
+        sortAdjustment: 1,
+        urlSubstring: "https://github.com/WebOfTrust/keripy/"
+    }
+];
