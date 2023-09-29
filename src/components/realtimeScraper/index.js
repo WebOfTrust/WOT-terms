cd4205f2a2cbcdec6a13a4d5d97ad3d01f3b652a
kor <kor@dwarshuis.com>
2023-09-29 21:03:49 +0200

    Rewrite of clumsy React code

--- src/components/realtimeScraper/index.js
+++ src/components/realtimeScraper/index.js
@@ -1,330 +1,228 @@
 import React, { useState, useEffect } from 'react';
-import Masonry from 'masonry-layout';
-import cheerio from 'cheerio';
-// import paths from "../../../docusaurus.paths";
-
-// Function to fetch GitHub commits and issues based on given URLs
-function fetchGitHubCommitsAndIssues(urls, callback) {
-  // Define the types of requests (commits and issues)
-  const types = ['commits', 'issues'];
-
-  // Create promises for each URL and each type (commit or issue)
-  const promises = urls.flatMap((url) =>
-    types.map((type) =>
-      new Promise((resolve) => {
-        // Perform a remote request for commits or issues
-        remoteRequest(`${url}/${type}`, (response) => {
-          // Process the HTML response to get needed data
-          const processedData = processData(response, url, type);
-          // Resolve the promise with the type and processed data
-          resolve({ type, data: processedData });
-        });
-      })
-    )
-  );
-
-  // Once all promises are resolved, proceed with the following
-  Promise.all(promises).then((results) => {
-    // Group the results by URL and type (commit or issue)
-    const groupedResults = results.reduce((acc, { type, data }) => {
-      const key = `${data.owner}/${data.repoName}`;
-      if (!acc[key]) acc[key] = {};
-      acc[key][type] = data;
-      return acc;
-    }, {});
-
-    // Generate JSX for the fetched data
-    let output = Object.entries(groupedResults).map(([key, value], index) => {
-      return (
-        <ul className="list-group" key={'parent' + index}>
-          <li className="list-group-item mt-1" key={'a' + index}>
-            <h3>{value.commits.repoName}</h3>
-            <a href={`https://github.com/${value.commits.detailsLink ? value.commits.detailsLink : value.issues.issuesLink}`}>
-              {value.commits.message ? "Latest commit: " + value.commits.owner : "issue: " + value.issues.owner}:
-              {value.commits.message ? value.commits.repoName : null},
-              {value.commits.message ? value.commits.h2Text : null},<br></br><br></br>
-              {value.issues.issuesText ? "Latest issue: " + value.issues.issuesText : null}
-            </a>
+// import Masonry from 'masonry-layout';
 
 
-          </li>
-        </ul >
-      )
-    });
-
-    // Call the callback function with the JSX output
-    callback(output);
-  });
-}
-
-// Function to process HTML responses and extract needed data
-function processData(response, url, type) {
-  // Initialize DOMParser
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(response, 'text/html');
-
-  // Debugging: Log header menu items (can be removed)
-  doc.querySelectorAll('.HeaderMenu-item').forEach((item) => {
-  });
-
-  // Extract repository owner and name from the URL
-  const urlParts = url.split('/');
-  const owner = urlParts[3];
-  const repoName = urlParts[4];
-
-  // Locate the first timeline item in the HTML document
-  const firstTimelineItem = doc.querySelector('.TimelineItem');
-
-  // Safely extract additional information from the HTML document
-  const h2Text = firstTimelineItem ? firstTimelineItem.querySelector('h2').innerText.trim() : "";
-  const detailsLinkElement = doc.querySelector('.Details p a.Link--primary');
-  const detailsLink = detailsLinkElement ? detailsLinkElement.getAttribute('href') : "";
-  const message = detailsLinkElement ? detailsLinkElement.innerText : "";
-  const commitAuthorElement = doc.querySelector('.commit-author');
-  const commitAuthor = commitAuthorElement ? commitAuthorElement.innerText : "";
-  const issuesElement = doc.querySelector('div[aria-label="Issues"] .Link--primary');
-  const issuesLink = issuesElement ? issuesElement.getAttribute('href') : "";
-  const issuesText = issuesElement ? issuesElement.innerText : "";
-
-  // Return processed data as an object
-  return {
-    type,
-    owner,
-    repoName,
-    h2Text,
-    detailsLink,
-    message,
-    commitAuthor,
-    issuesLink,
-    issuesText
-  };
-}
-
-const urls = [
+const urlsGithub = [
   'https://github.com/WebOfTrust/keripy',
   'https://github.com/WebOfTrust/cardano-backer',
   'https://github.com/WebOfTrust/cesride',
-  'https://github.com/WebOfTrust/keria'
+  'https://github.com/WebOfTrust/keria',
+  'https://github.com/WebOfTrust/signify-ts',
+  'https://github.com/WebOfTrust/signifypy'
 ];
 
 
-function fetchGithubInfo(url, callback) {
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      // Process the JSON data here
-      callback(data);
-    })
-    .catch(error => {
-      console.error("Error fetching commit information: ", error);
-    });
+// useEffect(() => {
+//   // Initialize Masonry
+//   const msnry = new Masonry('#masonry-grid', {
+//     "percentPosition": true,
+//     "itemSelector": '.mason-item',
+//     "initLayout": true
+//   });
+
+//   // You may also need to re-layout Masonry after each data update
+//   setTimeout(() => {
+//     msnry.layout()
+//   }, 2000);
+// }, []);
+
+
+// Helper function
+const fetchData = async (url) => {
+  const proxyUrl = 'https://dwarshuis.com/various/kerisse/proxy/proxy-curl.php?url=';
+  const res = await fetch(proxyUrl + url);
+
+  if (res.ok) {
+    return await res.text();
+  } else {
+    return 'Failed to fetch data';
+  }
+};
+
+// Initialize DOMParser
+const initializeDOMparser = (response) => {
+  if (typeof window !== 'undefined') {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(response, 'text/html');
+    return doc;
+  }
 }
 
-function remoteRequest(url, callback) {
-  var request = new XMLHttpRequest();
-  request.open("GET", "https://dwarshuis.com/various/kerisse/proxy/proxy-curl.php?url=" + url, true);
-  request.timeout = 30000;
-  request.onload = function () {
-    if (this.status >= 200 && this.status < 400) {
-      callback(this.response);
-    } else {
-      callback("Data cannot be shown.");
+const RealtimeScraperGithub = async (appendOutputData) => {
+  // loop through urlsGithub
+  for (const url of urlsGithub) {
+    const dataCommits = await fetchData(url + '/commits');
+    const documentCommits = initializeDOMparser(dataCommits);
+
+    const dataIssues = await fetchData(url + '/issues');
+    const documentIssues = initializeDOMparser(dataIssues);
+
+    // Extract repository owner and name from the URL
+    const urlParts = url.split('/');
+    const owner = urlParts[3];
+    const repoName = urlParts[4];
+
+    // only in client side rendering (browser) we can access the DOM elements. Webpack will give an error if we do not use an if .
+    if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+
+      // COMMITS
+      const firstTimelineItem = documentCommits.querySelector('.TimelineItem');
+      const commitDate = firstTimelineItem ? firstTimelineItem.querySelector('h2').innerText.trim() : "";
+      const commitLinkElement = firstTimelineItem.querySelector('.Details p a.Link--primary');
+      const commitLink = commitLinkElement ? commitLinkElement.getAttribute('href') : "";
+      const commitMessage = commitLinkElement ? commitLinkElement.innerText : "";
+      const commitAuthorElement = firstTimelineItem.querySelector('.commit-author');
+      const commitAuthor = commitAuthorElement ? commitAuthorElement.innerText : "";
+
+      // ISSUES
+      const issueElement = documentIssues.querySelector('div[aria-label="Issues"] .Link--primary');
+      const issueLink = issueElement ? issueElement.getAttribute('href') : "";
+      const issueMessage = issueElement ? issueElement.innerText : "";
+
+      // Append new information to the output data
+      appendOutputData(prevData => [...prevData, { owner, repoName, commitDate, commitLink, commitMessage, commitAuthor, issueLink, issueMessage }]);
     }
-  };
+  }
+};
 
-  request.onerror = function () {
-    callback("Data cannot be shown.");
-  };
-  request.send();
-}
+RealtimeScraperGithub();
 
+const IIW = () => {
+  const [response, setResponse] = useState('');
 
+  useEffect(() => {
+    const fetchFromProxy = async () => {
+      const response = await fetchData('https://internetidentityworkshop.com/schedule/');
+      const doc = initializeDOMparser(response);
+      console.log('doc: ', doc);
 
+      let selection;
+      if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+        selection = doc.querySelector('#section-1').innerText;
+        setResponse(selection);
+      }
+    };
 
-const fetchData = (url, callback) => {
-  switch (true) {
-    // case 'https://www.gleif.org/en/newsroom/gleif-and-lei-news/':
-    case url.includes('https://www.gleif.org/en/newsroom/gleif-and-lei-news'):
-      remoteRequest(url, (response) => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(response, 'text/html');
+    fetchFromProxy();
+  }, []);
 
-        let selection = doc.querySelectorAll('.news__item-headline');
+  return (
+    <div>
+      <h2>IIW Schedule</h2>
+      {/* <div dangerouslySetInnerHTML={{ __html: response }} /> */}
+      <p>{response}</p>
+    </div>
+  );
+};
+
+const Gleif = () => {
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const fetchFromProxy = async () => {
+      const response = await fetchData('https://www.gleif.org/en/newsroom/gleif-and-lei-news');
+      const doc = initializeDOMparser(response);
+
+      let selection;
+      if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+        selection = doc.querySelectorAll('.news__item-headline');
         selection = [...selection];
         selection = selection.map((element) => {
           return element;
         });
+      }
+      const listItems = selection.map((item, index) => {
+        if (index > 5) {
+          return;
+        }
+        return (
+          <ul className="list-group" key={'parent' + index}>
+            <li className='list-group-item mb-1 p-1' key={index}>
+              {item.innerText}
+            </li>
+          </ul>)
 
-        const listItems = selection.map((item, index) => {
-          if (index > 5) {
-            return;
-          }
-          return (
-            <ul className="list-group" key={'parent' + index}>
-              <li className='list-group-item mb-1 p-1' key={index}>
-                {item.innerText}
-              </li>
-            </ul>)
-
-        });
-
-
-
-        // setTimeout(msnry.layout, 100);
-        callback(listItems);
-        // Optionally, you can call customFunction here.
-        // customFunction(response);
-      });
-      break;
-    case url.includes('commits'):
-      fetchGithubInfo(url, (response) => {
-        const listItems = response.map((item, index) => {
-          if (index > 5) {
-            return;
-          }
-          return (
-            <ul className="list-group" key={'parent' + index}>
-              <li className="list-group-item" key={'a' + index}><a href={item.html_url}>{item.author.login}: {item.commit.message}</a></li>
-            </ul>
-          )
-        });
-        callback(listItems);
-      });
-      break;
-    case url.includes('internetidentityworkshop.com'):
-      remoteRequest(url, (response) => {
-        let $ = cheerio.load(response);
-
-        let selection = $('#section-1').text();
-
-        callback(selection);  // Make sure to call the callback function here
       });
 
-      break;
-  }
-};
-
-// function fetchIndexInfo() {
-//   // Fetching the HTML content
-//   fetch(paths.indexedInKERISSE)
-//     .then(response => response.text())
-//     .then(html => {
-//       // Parsing the fetched HTML string into a DOM object
-//       const parser = new DOMParser();
-//       const doc = parser.parseFromString(html, 'text/html');
-
-//       // Finding the paragraph elements by their id's
-//       const timestampElement = doc.querySelector('#index-created-timestamp');
-//       const pageCountElement = doc.querySelector('#index-created-page-count');
-
-//       if (timestampElement) {
-//         // Extracting and logging the content of the paragraph
-//         const timestampContent = timestampElement.textContent;
-//         document.querySelector('#index-created-timestamp').textContent = timestampContent;
-//       } else {
-//         console.log('Element with id "index-created-timestamp" not found.');
-//       }
-
-//       if (pageCountElement) {
-//         // Extracting and logging the content of the paragraph
-//         const pageCountContent = pageCountElement.textContent;
-//         document.querySelector('#index-created-page-count').textContent = pageCountContent;
-//       } else {
-//         console.log('Element with id "index-created-page-count" not found.');
-//       }
-//     })
-//     .catch(error => {
-//       console.error(`Error fetching the content: ${error}`);
-//     });
-//   // END TIMESTAMP
-
-
-// }
-
-const RealTimeScraping = () => {
-  const [data1, setData1] = useState(null);
-  const [data2, setData2] = useState(null);
-  const [data4, setData4] = useState(null);
-  const [data5, setData5] = useState(null);
-  const [data6, setData6] = useState(null);
-
-  useEffect(() => {
-    fetchData('https://www.gleif.org/en/newsroom/gleif-and-lei-news/', setData1);
+      setResponse(listItems);
+    };
+    fetchFromProxy();
   }, []);
 
-  // useEffect(() => {
-  //   fetchData('https://api.github.com/repos/WebOfTrust/keripy/commits', setData2);
-  // }, []);
-
-  useEffect(() => {
-    fetchData('https://internetidentityworkshop.com/schedule/', setData4);
-  }, []);
-
-  useEffect(() => {
-    fetchGitHubCommitsAndIssues(urls, setData5);
-  }, []);
-
-  // useEffect(() => {
-  //   fetchIndexInfo(setData6);
-  // }, []);
-
-
-
-  useEffect(() => {
-    // Initialize Masonry
-    const msnry = new Masonry('#masonry-grid', {
-      "percentPosition": true,
-      "itemSelector": '.mason-item',
-      "initLayout": true
-    });
-
-    // You may also need to re-layout Masonry after each data update
-    setTimeout(() => {
-      msnry.layout()
-    }, 2000);
-  }, [data1, data2, data4]);
 
   return (
+    <div>
+      <h2>Gleif news</h2>
+      {/* <div dangerouslySetInnerHTML={{ __html: response }} /> */}
+      <div>{response}</div>
+    </div>
+  );
+};
+
+
+const GithubFinalOutput = ({ outputData }) => {
+  return (
+    <ul className="list-group">
+      {outputData.map((data, index) => (
+        <li key={index} className='list-group-item mb-1 p-1'>
+          <h3 key={index}>{data.owner} – {data.repoName}</h3>
+          <h4>{data.commitDate}</h4>
+          <p><a href={`https://github.com/${data.commitLink}`}>{data.commitMessage} ({data.commitAuthor})</a></p>
+          {/* <p>Author: {data.commitAuthor}</p> */}
+          {data.issueMessage && (
+            <>
+              <h4>Latest issue:</h4>
+              <p><a href={`https://github.com/${data.issueLink}`}>{data.issueMessage}</a></p>
+            </>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+
+const GithubScraper = () => {
+  const [outputData, setOutputData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await RealtimeScraperGithub(setOutputData); // Pass setOutputData to RealtimeScraperGithub
+    };
+
+    fetchData();
+  }, []);
+
+  return <GithubFinalOutput outputData={outputData} />;
+}
+
+
+const RealtimeScraper = () => {
+  return (
     <div className="container text-start mt-5 realtime">
-      <div style={{ fontSize: '0.7em' }} className="row" id="masonry-grid">
-        <div className="col-lg-4 col-md-6 mb-2 mason-item">
+      <div className="row" id="masonry-grid">
+        <div className="col-lg-6 col-md-6 mb-2 mason-item">
           <div className="card">
             <div className="card-body">
-              <h2>Gleif latest news</h2>
-              {data1}
-              <p className="mt-2">Go to <a target='_blank' rel="noopener" href="https://www.gleif.org/en/newsroom/gleif-and-lei-news/">latest news</a></p>
+              <h2>Github</h2>
+              <GithubScraper />
             </div>
           </div>
         </div>
-        {/* <div className="col-lg-4 col-md-6 mb-2 mason-item">
+        <div className="col-lg-6 col-md-6 mb-2 mason-item">
           <div className="card">
             <div className="card-body">
-              <h2>Info</h2>
-              {data6}
+              <IIW />
             </div>
           </div>
-        </div> */}
-        <div className="col-lg-4 col-md-6 mb-2 mason-item">
-          <div className="card">
+          <div className="card mt-2">
             <div className="card-body">
-              <h2>IIW schedule</h2>
-              {data4}
-              <p className="mt-2">Go to <a target='_blank' rel="noopener" href="https://internetidentityworkshop.com/schedule/">schedule</a></p>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-4 col-md-6 mb-2 mason-item">
-          <div className="card">
-            <div className="card-body">
-              <h2>Repos</h2>
-              {data5}
+              <Gleif />
             </div>
           </div>
         </div>
       </div>
     </div>
-
   );
 };
-
-export default RealTimeScraping;
+export default RealtimeScraper;
