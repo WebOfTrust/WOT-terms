388bedd0094503f0b758804494d6661188438bc0
kor <kor@dwarshuis.com>
2023-10-01 21:10:08 +0200

    Better full size images

--- clientModules/imageFullSize.js
+++ clientModules/imageFullSize.js
@@ -11,21 +11,53 @@
 */
 
 const imageFullSize = () => {
-  const imageFullSizeClass = 'full-page-image';
+  const imageFullSizeClass = 'image-full-page';
 
   // All images get a click event listener via event delegation
-  const markdownElement = document.querySelector('.markdown');
+  const markdownElement = document.querySelector('body');
+
+  function removeContainer() {
+    let container = document.querySelector('.image-container-full-page');
+    if (container) {
+      document.body.removeChild(container);
+    }
+  }
 
   if (markdownElement) {
     markdownElement.addEventListener('click', (event) => {
-      // If the clicked element is an image
       if (event.target.tagName === 'IMG') {
-        console.log("clicked image");
+        let image = event.target;
 
-        // Toggle the full size class
-        event.target.classList.toggle(imageFullSizeClass);
+        if (document.querySelector('.image-container-full-page') === null) {
+          // Add container if it doesn't exist
+
+          // Clone the image element
+          const clonedImage = image.cloneNode(true);
+
+          // Create a new div element with the class .full-page-image-container
+          const container = document.createElement('div');
+          container.classList.add('image-container');
+          container.classList.add('image-container-full-page');
+
+          // Add the cloned image to the new container div
+          container.appendChild(clonedImage);
+
+          // Append the new container div to the body
+          document.body.appendChild(container);
+
+          // Add event listener to the new container
+          container.addEventListener('click', function containerClickEvent() {
+            container.removeEventListener('click', containerClickEvent); // remove this click listener
+            removeContainer();
+          });
+
+        }
+
+        // Toggle the full size class on the original image
+        image.classList.toggle(imageFullSizeClass);
       }
     });
+
   } else {
     console.log("Element with class '.markdown' not found.");
   }
