30f0c9adce62af1de54da4b1cff4fa9e4007375f
kor <kor@dwarshuis.com>
2023-10-06 11:18:16 +0200

    New test

--- search-index-typesense/scrape-start-test.sh
+++ search-index-typesense/scrape-start-test.sh
@@ -19,31 +19,31 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # PREPARING
 #########################
 
-# Prepare file system.
-source "$SCRIPT_DIR/prepare_file_system.sh"
-setLogFile "success.log"
-log "Preparing file system finished"
+# # Prepare file system.
+# source "$SCRIPT_DIR/prepare_file_system.sh"
+# setLogFile "success.log"
+# log "Preparing file system finished"
 
-# Copy handmade stuff.
-source "$SCRIPT_DIR/copy_manual_files.sh"
-setLogFile "success.log"
-log "Copying manual files finished"
+# # Copy handmade stuff.
+# source "$SCRIPT_DIR/copy_manual_files.sh"
+# setLogFile "success.log"
+# log "Copying manual files finished"
 
-# Create sitemaps.
-source "$SCRIPT_DIR/create_sitemaps-test.sh"
-setLogFile "success.log"
-log "Creating sitemaps finished"
+# # Create sitemaps.
+# source "$SCRIPT_DIR/create_sitemaps-test.sh"
+# setLogFile "success.log"
+# log "Creating sitemaps finished"
 
-# Remove unwanted urls from the sitemaps (new sitemaps generated or not)
-node "$SCRIPT_DIR/removeURLsFromSitemap.mjs"
-setLogFile "success.log"
-log "Extracting data finished"
+# # Remove unwanted urls from the sitemaps (new sitemaps generated or not)
+# node "$SCRIPT_DIR/removeURLsFromSitemap.mjs"
+# setLogFile "success.log"
+# log "Extracting data finished"
 
 
-# Filenames to lowercase.
-node "${SCRIPT_DIR}/renameFilesToLowerCase.mjs" ${SEARCH_INDEX_DIR}/sitemaps
-setLogFile "success.log"
-log "Renaming files to lowercase finished"
+# # Filenames to lowercase.
+# node "${SCRIPT_DIR}/renameFilesToLowerCase.mjs" ${SEARCH_INDEX_DIR}/sitemaps
+# setLogFile "success.log"
+# log "Renaming files to lowercase finished"
 
 
 
@@ -51,22 +51,22 @@ log "Renaming files to lowercase finished"
 # START SCRAPING
 #########################
 
-# Scrape the websites.
-node "$SCRIPT_DIR/extractData-test.mjs"
-setLogFile "success.log"
-log "Extracting data finished"
+# # Scrape the websites.
+# node "$SCRIPT_DIR/extractData-test.mjs"
+# setLogFile "success.log"
+# log "Extracting data finished"
 
-# Split the content.jsonl file into multiple files so the size is optimal for Typesense.
-node "$SCRIPT_DIR/splitContentJSONL.mjs"
-setLogFile "success.log"
-log "Splitting content finished"
+# # Split the content.jsonl file into multiple files so the size is optimal for Typesense.
+# node "$SCRIPT_DIR/splitContentJSONL.mjs"
+# setLogFile "success.log"
+# log "Splitting content finished"
 
-# Count the total number of lines in all .jsonl files and write it to log dir.
-node "$SCRIPT_DIR/countLinesInJsonlFiles.mjs"
-setLogFile "success.log"
-log "Counting number of lines finished"
+# # Count the total number of lines in all .jsonl files and write it to log dir.
+# node "$SCRIPT_DIR/countLinesInJsonlFiles.mjs"
+# setLogFile "success.log"
+# log "Counting number of lines finished"
 
-node "$SCRIPT_DIR/collectScrapedUrls.mjs" "${SEARCH_INDEX_DIR}/${SEARCH_INDEX_ENTRIES_DIR}" "$INDEX_OVERVIEW_FILE"
+node "$SCRIPT_DIR/collectScrapedUrls.mjs" "${SEARCH_INDEX_DIR}/${SEARCH_INDEX_ENTRIES_DIR}" "${INDEX_OVERVIEW_FILE}"
 setLogFile "success.log"
 log "Collecting urls and writing to index file finished"
 
@@ -77,19 +77,19 @@ log "Sorting and styling index file finished"
 
 
 
-#########################
-# BACKING UP
-#########################
+# #########################
+# # BACKING UP
+# #########################
 
 # # Export the data from Typesense to the downloads dir.
 # source "$SCRIPT_DIR/export.sh"
 # setLogFile "success.log"
 # log "Exporting data finished"
 
-# Backup output (scrape results, handmade stuff, sitemaps etc).
-source "$SCRIPT_DIR/backup.sh"
-setLogFile "success.log"
-log "Backup finished"
+# # Backup output (scrape results, handmade stuff, sitemaps etc).
+# source "$SCRIPT_DIR/backup.sh"
+# setLogFile "success.log"
+# log "Backup finished"
 
 
 
@@ -112,6 +112,6 @@ log "Backup finished"
 # setLogFile "success.log"
 # log "Importing overrides finished"
 
-# #########################
-# # END
-# #########################
+#########################
+# END
+#########################
