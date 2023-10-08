c0a9802a641367c4cacec31317a3059a214dad90
kor <kor@dwarshuis.com>
2023-10-08 18:46:21 +0200

    Removed unused code

--- search-index-typesense/import.sh
+++ search-index-typesense/import.sh
@@ -37,27 +37,12 @@ urlDelete="https://${local_TYPESENSE_HOST}.a1.typesense.net/collections/${local_
 # URL of the endpoint to import documents
 urlImport="https://${local_TYPESENSE_HOST}.a1.typesense.net/collections/${local_TYPESENSE_COLLECTION_NAME}/documents/import?action=create"
 
-# Handmade entries
-input_handmade_dir="$(pwd)/${SEARCH_INDEX_DIR}/search-index-entries-manual"
-output_handmade_dir="$(pwd)/${SEARCH_INDEX_DIR}/search-index-entries-manual"
-
 # Automated entries
 input_dir="$(pwd)/${SEARCH_INDEX_DIR}/${SEARCH_INDEX_ENTRIES_DIR}"
 output_dir="$(pwd)/${SEARCH_INDEX_DIR}/${SEARCH_INDEX_ENTRIES_DIR}"
 
 # log files
 log_dir="$(pwd)/${SEARCH_INDEX_DIR}/logs"
-
-
-
-
-# ############## COPY FROM HANDMADE DIR TO (MAIN) OUTPUT DIR ##############
-# # Copy all .json files from the output-handmade directory to the output directory, so they will be converted to jsonl as well together with the automated entries
-# for file in "$output_handmade_dir"/*.json; do
-#     cp "$file" "$output_dir"
-# done
-
-
 
 
 ############## CONVERT JSON TO JSONL ##############
