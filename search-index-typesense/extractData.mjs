20607427b51eba836bab904ee617009e56e2b37a
kor <kor@dwarshuis.com>
2023-10-20 13:56:12 +0200

    New config

--- search-index-typesense/extractData.mjs
+++ search-index-typesense/extractData.mjs
@@ -14,13 +14,14 @@ import scrapeGleif from './custom-scrapers/gleif.mjs';
 
 import scrapeWotgloss from './custom-scrapers/wotgloss.mjs';
 import scrapeESSIFLab from './custom-scrapers/eSSIF-Lab.mjs';
-import scrapeJolocom from './custom-scrapers/jolocom.io.mjs';
-import scrapeHumancolossus from './custom-scrapers/humancolossus.mjs';
-import scrapeKsoeteman from './custom-scrapers/ksoeteman.mjs';
-import scrapemediumCom from './custom-scrapers/medium.com.mjs';
-import scrapeKentbull from './custom-scrapers/kentbull.com.mjs';
+// import scrapeJolocom from './custom-scrapers/jolocom.io.mjs';
+// import scrapeHumancolossus from './custom-scrapers/humancolossus.mjs';
+// import scrapeKsoeteman from './custom-scrapers/ksoeteman.mjs';
+// import scrapemediumCom from './custom-scrapers/medium.com.mjs';
+// import scrapeKentbull from './custom-scrapers/kentbull.com.mjs';
 import scrapeGithub from './custom-scrapers/github.mjs';
 import scrapeReadthedocs from './custom-scrapers/readthedocs.io.mjs';
+import scraperGeneric from './modules/scraper-generic.mjs';
 
 scrapeWOTterms();
 scrapeGleif();
@@ -30,10 +31,11 @@ scrapeGleif();
 
 scrapeWotgloss();
 scrapeESSIFLab();
-scrapeJolocom();
-scrapeHumancolossus();
-scrapeKsoeteman();
-scrapemediumCom();
-scrapeKentbull();
+// scrapeJolocom();
+// scrapeHumancolossus();
+// scrapeKsoeteman();
+// scrapemediumCom();
+// scrapeKentbull();
 scrapeGithub();
 scrapeReadthedocs();
+scraperGeneric();
