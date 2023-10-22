37f720c1e767c71ca0cab19f741a5d66d21352e9
kor <kor@dwarshuis.com>
2023-10-22 21:55:11 +0200

    test: copy of extractData.mjs for testing

---
+++ search-index-typesense/extractData-test.mjs
@@ -0,0 +1,14 @@
+/*
+  Author: Kor Dwarshuis
+  Created: 2023-03-16
+  Updated: -
+  Description: 
+*/
+
+// import scraperGithub from './scrapers-config/scraper-github.mjs';
+// import scraperGenericSingleUrls from './scrapers-config/scraper-generic-single-urls.mjs';
+import scraperGenericSitemapTest from './scrapers-config/scraper-generic-sitemaps-test.mjs';
+
+// scraperGithub();
+// scraperGenericSingleUrls();
+scraperGenericSitemapTest();
