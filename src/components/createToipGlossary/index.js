040a49e6812a0fbeb9eb95aaf3087ce544503965
kor <kor@dwarshuis.com>
2023-11-01 06:16:33 +0100

    fix: each child in the list now has a unique "key" prop.

--- src/components/createToipGlossary/index.js
+++ src/components/createToipGlossary/index.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 const CreateToipGlossary = () => {
-    const [terms, setTerms] = useState([]);
     const termsData = require('@site/static/json/terms-definitions-toip.json');
 
     return (
         <dl>
             {termsData.map((term) => (
-                <>
+                <React.Fragment key={term.term}>
                     <dt>{term.term}</dt>
                     <dd className='ms-5' dangerouslySetInnerHTML={{ __html: term.definition }}></dd>
-                </>
+                </React.Fragment>
             ))}
         </dl>
     );
