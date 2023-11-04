dc0d4564709873ba23c25466f16b53e55efa4e82
kor <kor@dwarshuis.com>
2023-11-04 08:25:57 +0100

    refactor: add bootstrap classes via a const

--- clientModules/addUiToSidebar.js
+++ clientModules/addUiToSidebar.js
@@ -127,20 +127,20 @@ const addUiToSidebar = () => {
         return;
     }
     const nav = document.querySelector('nav[aria-label="Docs sidebar"]');
-
+    const bootstrapClasses = `check-container-form', 'border', 'ms-2', 'me-2', 'mb-2', 'p-2', 'rounded'`;
 
     const checkboxFormContainer = document.createElement('div');
-    checkboxFormContainer.classList.add('check-container-form', 'border', 'ms-2', 'me-2', 'mb-2', 'p-2', 'rounded');
+    checkboxFormContainer.classList.add(bootstrapClasses);
     checkboxFormContainer.style.fontSize = '0.8rem';
     checkboxFormContainer.innerHTML = `<h2 style="font-size: 1.2em">Form:</h2>`
 
     const checkboxLevelContainer = document.createElement('div');
-    checkboxLevelContainer.classList.add('check-container-level', 'border', 'ms-2', 'me-2', 'mb-2', 'p-2', 'rounded');
+    checkboxLevelContainer.classList.add(bootstrapClasses);
     checkboxLevelContainer.style.fontSize = '0.8rem';
     checkboxLevelContainer.innerHTML = `<h2 style="font-size: 1.2em">Level:</h2>`
 
     const checkboxTypeContainer = document.createElement('div');
-    checkboxTypeContainer.classList.add('check-container-type', 'border', 'ms-2', 'me-2', 'mb-2', 'p-2', 'rounded');
+    checkboxTypeContainer.classList.add(bootstrapClasses);
     checkboxTypeContainer.style.fontSize = '0.8rem';
     checkboxTypeContainer.innerHTML = `<h2 style="font-size: 1.2em">Type:</h2>`
 
