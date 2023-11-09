05f52ed4b30ae144e200f5722fe3d14cae5d0d88
kor <kor@dwarshuis.com>
2023-11-09 08:01:15 +0100

    test: try a[href$= instead of a[href= since it doesn't work in iOS Safari

--- clientModules/addUiToSidebar.js
+++ clientModules/addUiToSidebar.js
@@ -336,7 +336,10 @@ export function onRouteDidUpdate({ location, previousLocation }) {
     // const glossaryMainMenuItem = document.querySelector(`.theme-doc-sidebar-menu li a[href="${baseUrl}docs/glossary"]`);
     console.log('baseUrl: ', baseUrl);
     // const selectorString = '.theme-doc-sidebar-menu li a[href="' + baseUrl + 'docs/glossary"]';
-    const selectorString = ".theme-doc-sidebar-menu li a[href='" + baseUrl + "docs/glossary']";
+    // const selectorString = ".theme-doc-sidebar-menu li a[href='" + baseUrl + "docs/glossary']";
+    // const selectorString = ".theme-doc-sidebar-menu li a[href='" + baseUrl + "docs/glossary']";
+    document.querySelector(".theme-doc-sidebar-menu li a[href$='" + baseUrl + "docs/glossary']")
+
     console.log('selectorString: ', selectorString);
     const glossaryMainMenuItem = document.querySelector(selectorString);
     console.log('glossaryMainMenuItem: ', glossaryMainMenuItem);
