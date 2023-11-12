1d6ee3ec35b63b953c6bc22913e6388ab1e1dae8
kor <kor@dwarshuis.com>
2023-11-12 21:52:14 +0100

    feat: Start 4 node scripts

---
+++ fetchExternalContent/fetchMentalModels/main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+node fetchExternalContent/fetchMentalModels/fetchToIPContent/fetchToIPContent.js
+
+node fetchExternalContent/fetchMentalModels/fetchNistContent/fetchNistContent.mjs
+
+node fetchExternalContent/fetchMentalModels/fetchEssifLabContent/fetchEssifLabContent.js
+
+node fetchExternalContent/fetchMentalModels/fetchDigitalGovtNzContent/fetchDigitalGovtNzContent.js
