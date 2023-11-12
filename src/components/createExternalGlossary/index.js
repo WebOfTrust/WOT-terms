67800df6f4cffe76e838ca808f48de5434ac7225
kor <kor@dwarshuis.com>
2023-11-12 21:31:20 +0100

    feat: Add id for styling

--- src/components/createExternalGlossary/index.js
+++ src/components/createExternalGlossary/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const createExternalGlossary = ({ termsData }) => {
     return (
-        <dl>
+        <dl id='glossary_content'>
             {termsData.map((term) => (
                 <React.Fragment key={term.term}>
                     <dt><a id={term.anchor}>{term.term}</a></dt>
-                    <dd className='ms-5' dangerouslySetInnerHTML={{ __html: term.definition }}></dd>
+                    <dd dangerouslySetInnerHTML={{ __html: term.definition }}></dd>
                 </React.Fragment>
             ))}
         </dl>
