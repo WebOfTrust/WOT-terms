c8670fc93d6b2bb8ff456061c9fc9934605dfff6
kor <kor@dwarshuis.com>
2023-11-12 18:58:44 +0100

    improve: better console.log message

--- fetchExternalContent/fetchMentalModels/fetchNistContent/fetchNistContent.mjs
+++ fetchExternalContent/fetchMentalModels/fetchNistContent/fetchNistContent.mjs
@@ -1,38 +1,59 @@
 import downloadFile from '../../../modules-js-node/downloadFile.mjs';
 import unzipFile from '../../../modules-js-node/unzipFile.mjs';
-
-// use require to get downloadFile
-// const downloadFile = require('../../../modules-js-universal/downloadFile.mjs');
-console.log('downloadFile: ', downloadFile);
-
-
-
-// import { unzipFile } from './unzipFile.mjs';
-
-
-
-// const axios = require('axios');
-// const fs = require('fs');
-// const path = require('path');
 import path from 'path';
-// const { JSDOM } = require('jsdom');
-// require('dotenv').config();
+import fs from 'fs';
+import { config as configDotEnv } from 'dotenv';
+configDotEnv();
 
 // CONFIG
 const url = 'https://csrc.nist.gov/csrc/media/glossary/glossary-export.zip';
-const dir = 'fetchExternalContent/fetchMentalModels/fetchNistContent/download'
+const dir = './fetchExternalContent/fetchMentalModels/fetchNistContent/download'
 const filename = 'glossary-export.zip';
 const fullPath = path.join(dir, filename); // Use path.join for better compatibility
+const overview = './static/json/overview.json';
 
-// You have to figure out the URL to the download via the network panel.
-// Find it by going to File - Download in the Google Docs menu: 
-// https://docs.google.com/document/d/1fZByfuSOwszDRkE7ARQLeElSYmVznoOyJK4sxRvJpyM/edit
-const unzippedFilename = 'glossary-export.json';
-const generatedJSONfilename = 'terms-definitions-toip.json';
+const sourceJsonFullPath = dir + '/' + 'glossary-export.json'; ``
+const filteredJsonFullPath = path.join(process.env.GENERATED_JSON_DIR, 'terms-definitions-nist.json');
+
 // END CONFIG
 
 
 
+function filterJson(overviewPath, sourceJsonPath, filteredJsonPath) {
+    try {
+        // Read and parse the overview JSON
+        const overviewData = JSON.parse(fs.readFileSync(overviewPath, 'utf8'));
+        const terms = overviewData.values.slice(1).map(row => row[5]);
+
+        // Read and parse the source JSON, with additional error handling for BOM
+        let sourceJsonRaw = fs.readFileSync(sourceJsonPath, 'utf8');
+        if (sourceJsonRaw.charCodeAt(0) === 0xFEFF) {
+            sourceJsonRaw = sourceJsonRaw.slice(1); // Remove BOM
+        }
+        const sourceJsonData = JSON.parse(sourceJsonRaw);
+
+        // Filter the terms, assign the appropriate 'definition', add 'organisation', and rename 'link' to 'url'
+        const filteredTerms = sourceJsonData.parentTerms.filter(termObj => terms.includes(termObj.term)).map(termObj => {
+            if (termObj.definitions && termObj.definitions.length > 0 && termObj.definitions[0].text) {
+                termObj.definition = termObj.definitions[0].text;
+            } else {
+                termObj.definition = "Term found but the definition does not exist yet.";
+            }
+            termObj.organisation = "Nist";
+            if (termObj.link) {
+                termObj.url = termObj.link; // Rename 'link' to 'url'
+                delete termObj.link; // Remove the old 'link' property
+            }
+            delete termObj.definitions;
+            return termObj;
+        });
+
+        // Write only the filteredTerms array to the file
+        fs.writeFileSync(filteredJsonPath, JSON.stringify(filteredTerms, null, 4));
+    } catch (error) {
+        console.error(`An error occurred: ${error.message}`);
+    }
+}
 
 
 // Main function to run the tasks sequentially
@@ -46,7 +67,11 @@ async function main() {
         // to complete before moving on to the next operation.
         // Therefore, using await is not necessary in this specific instance.
         unzipFile(fullPath, dir); // Then unzip
-        console.log('Download and unzip completed');
+
+        // Usage
+        filterJson(overview, sourceJsonFullPath, filteredJsonFullPath);
+
+        console.log('Download, unzip and processing completed');
     } catch (err) {
         console.error('An error occurred:', err);
     }
