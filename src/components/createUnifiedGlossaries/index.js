6fbad067642896be3e7b7a395de1af820e94970d
kor <kor@dwarshuis.com>
2023-11-17 12:01:11 +0100

    feat: Filter for organisations

--- src/components/createUnifiedGlossaries/index.js
+++ src/components/createUnifiedGlossaries/index.js
@@ -2,22 +2,7 @@ import React, { useState, useEffect } from 'react';
 import parse from 'html-react-parser';
 const termsData = require('@site/static/json/external-glosseries/glossaries-combined/all-glossaries.json');
 
-// // Create an index mapping each alphabet letter to the first term's anchor starting with that letter
-// const alphabetIndex = {};
-
-// termsData.forEach(term => {
-//     const firstLetter = term.term.charAt(0).toUpperCase();
-//     if (!alphabetIndex[firstLetter]) {
-//         alphabetIndex[firstLetter] = term.anchor;
-//     }
-// }
-// );
-
-// // Create an array of alphabet letters (A-Z)
-// const alphabetLetters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
-
-
-// Go through termsDate.definitions.definition and and replace <h2> with <h4> and <h3> with <h5> etc.
+// Update the structure of termsData to replace tags as needed
 termsData.forEach(term => {
     term.definitions.forEach(definition => {
         definition.definition = definition.definition
@@ -29,44 +14,79 @@ termsData.forEach(term => {
 
 const CreateUnifiedGlossaries = () => {
     const [searchTerm, setSearchTerm] = useState('');
-
-    const handleSearch = (event) => {
-        setSearchTerm(event.target.value);
-    };
+    const [organisations, setOrganisations] = useState([]);
+    const [checkedOrganisations, setCheckedOrganisations] = useState({});
 
     useEffect(() => {
-        const listItems = document.querySelectorAll('.term h2');
-        listItems.forEach(item => {
-            // If the item text includes the search term, remove 'd-none', else add 'd-none'
-            if (item.textContent.toLowerCase().includes(searchTerm.toLowerCase())) {
-                item.parentElement.classList.remove('d-none');
-                console.log(item.textContent);
-            } else {
-                item.parentElement.classList.add('d-none');
-            }
+        const allOrganisations = new Set();
+        termsData.forEach(term => {
+            term.definitions.forEach(definition => {
+                allOrganisations.add(definition.organisation);
+            });
         });
-    }, [searchTerm]);
+
+        const orgArray = Array.from(allOrganisations);
+        setOrganisations(orgArray);
+
+        const initialCheckedState = {};
+        orgArray.forEach(org => {
+            initialCheckedState[org] = true;
+        });
+        setCheckedOrganisations(initialCheckedState);
+    }, []);
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value.toLowerCase());
+    };
+
+    const handleCheckboxChange = (organisation) => {
+        setCheckedOrganisations(prevChecked => ({
+            ...prevChecked,
+            [organisation]: !prevChecked[organisation]
+        }));
+    };
+
+    const isTermVisible = (term) => {
+        return term.definitions.some(definition =>
+            checkedOrganisations[definition.organisation] && term.term.toLowerCase().includes(searchTerm)
+        );
+    };
 
     return (
         <div>
             <input type="text" className="form-control" placeholder="Search" aria-label="Search" onChange={handleSearch} />
-
+            <div className='mt-5 mb-5 text-center'>
+                {organisations.map((organisation, index) => (
+                    <div className="form-check form-check-inline" key={index}>
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            checked={checkedOrganisations[organisation]}
+                            onChange={() => handleCheckboxChange(organisation)}
+                            id={`checkbox-${organisation}`}
+                        />
+                        <label className="form-check-label" htmlFor={`checkbox-${organisation}`}>
+                            {organisation}
+                        </label>
+                    </div>
+                ))}
+            </div>
             <ul className="list-group">
                 {termsData.map((term, index) => (
-                    <li className="term list-group-item border border-0" key={index}>
-                        <h2 id={term.anchor}>{term.term} <a href={`#${term.anchor}`} className="hash-link" aria-label={`Direct link to ${term.anchor}`} title={`Direct link to ${term.anchor}`}>​</a></h2>
+                    <li className={`term list-group-item border border-0 ${isTermVisible(term) ? '' : 'd-none'}`} key={index}>
+                        <h2 id={term.anchor}>{term.term}</h2>
                         <ul className="list-group">
-                            {term.definitions.map((definition, index) => (
-                                <li className="list-group-item border border-0" key={index}>
+                            {term.definitions.map((definition, defIndex) => (
+                                <li className={`list-group-item border border-0 ${checkedOrganisations[definition.organisation] ? '' : 'd-none'}`} key={defIndex}>
                                     <div className="card">
                                         <div className="card-header">
-                                            <h3 className="card-title">{definition.organisation}</h3>
+                                            <h3 className="card-title organisation">{definition.organisation}</h3>
                                         </div>
                                         <div className="card-body">
                                             <div className="card-text">{parse(definition.definition)}</div>
                                         </div>
                                         <div className="card-footer">
-                                            <a href={definition.url} className="">Learn more</a>
+                                            <a href={definition.url}>Learn more</a>
                                         </div>
                                     </div>
                                 </li>
