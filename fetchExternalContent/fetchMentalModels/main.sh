269b89610d939c37a6946f3e89a992e222d157fa
kor <kor@dwarshuis.com>
2023-12-03 14:14:16 +0100

    chore: Update content

--- fetchExternalContent/fetchMentalModels/main.sh
+++ fetchExternalContent/fetchMentalModels/main.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-node fetchExternalContent/fetchMentalModels/fetchToIPContent/fetchToIPContent.js
-
-node fetchExternalContent/fetchMentalModels/fetchNistContent/fetchNistContent.mjs
+node fetchExternalContent/fetchMentalModels/fetchDigitalGovtNzContent/fetchDigitalGovtNzContent.js
 
 node fetchExternalContent/fetchMentalModels/fetchEssifLabContent/fetchEssifLabContent.js
 
-node fetchExternalContent/fetchMentalModels/fetchDigitalGovtNzContent/fetchDigitalGovtNzContent.js
+node fetchExternalContent/fetchMentalModels/fetchNistContent/fetchNistContent.mjs
 
-node fetchExternalContent/fetchMentalModels/fetchW3cDid/fetchW3cDid.js
+node fetchExternalContent/fetchMentalModels/fetchToIPContent/fetchToIPContent.js
 
 node fetchExternalContent/fetchMentalModels/fetchToipDidWebs/fetchToipDidWebs.js
 
+node fetchExternalContent/fetchMentalModels/fetchW3cDid/fetchW3cDid.js
+
+# Combine glossaries
 node fetchExternalContent/fetchMentalModels/combineGlossaries.js
