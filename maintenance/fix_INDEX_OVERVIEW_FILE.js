cdbeb35e97e372832f357c526ec78de6d092b2ce
kor <kor@dwarshuis.com>
2023-12-04 19:48:31 +0100

    feat: Replace “%E2%80%90” with “-”

---
+++ maintenance/fix_INDEX_OVERVIEW_FILE.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const fs = require('fs');
+
+// load INDEX_OVERVIEW_FILE from .env
+const indexOverviewFile = process.env.INDEX_OVERVIEW_FILE;
+const indexOverviewFileContent = fs.readFileSync(indexOverviewFile, 'utf8');
+console.log('indexOverviewFileContent: ', indexOverviewFileContent);
+
+// Clean up the file content
+
+// Via regex replace %E2%80%90 with -
+const regex = /%E2%80%90/gm;
+const fixedIndexOverviewFileContent = indexOverviewFileContent.replace(regex, '-');
+console.log('fixedIndexOverviewFileContent: ', fixedIndexOverviewFileContent);
+
+// Write the fixed content back to the file
+fs.writeFileSync(indexOverviewFile, fixedIndexOverviewFileContent, 'utf8');
