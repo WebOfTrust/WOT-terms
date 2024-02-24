6fe675806575191d7818dbb7b5dbc6c73acc8f84
kor <kor@dwarshuis.com>
2024-02-24 20:26:57 +0100

    feat: better styling of results

--- maintenance/findBrokenLinks.js
+++ maintenance/findBrokenLinks.js
@@ -109,13 +109,16 @@ const siteChecker = new SiteChecker({
 
         // Format the output for the Markdown file
         let dataToWrite = `# Broken Links Report\n\nCreated: ${new Date().toISOString()}\n\n`;
-        dataToWrite += `## Total Broken Links Found: ${numberOfBrokenLinks}\n\n`;
+        dataToWrite += `Total Broken Links Found: ${numberOfBrokenLinks}\n\n`;
+
+        let counter = 1; // Initialize counter variable outside the loop
 
         for (const [brokenLink, foundOnPages] of Object.entries(brokenLinks)) {
             let markdownBrokenLink = `[${brokenLink}](${brokenLink})`;
-            let pagesMarkdown = foundOnPages.map(page => `[${page}](${page})`).join('\n\n\n');
-
-            dataToWrite += `- Broken Link:\n${markdownBrokenLink}\n\nFound on Pages: ${pagesMarkdown}\n`;
+            let pagesMarkdown = foundOnPages.map(page => `[${page}](${page})`).join('\n');
+            pagesMarkdown += '\n\n';
+            dataToWrite += `## Broken Link #${counter}:\n${markdownBrokenLink}\n\nFound on Pages:\n${pagesMarkdown}\n`;
+            counter++; // Increment counter for the next broken link
         }
 
         // Now, dataToWrite is a Markdown string where every URL is clickable
@@ -142,24 +145,24 @@ const siteChecker = new SiteChecker({
                 body: "Created: " + timestamp + "\n\n" + "Number of broken internal links: " + numberOfBrokenLinks + "\n\n" + "<a href='https://github.com/WebOfTrust/WOT-terms/blob/main/logs/brokenLinks.md'>See full list of broken internal links</a>.",
             };
 
-            const octokit = new Octokit({
-                auth: config.githubToken
-            });
+            // const octokit = new Octokit({
+            //     auth: config.githubToken
+            // });
 
-            octokit.request('POST /repos/WebOfTrust/WOT-terms/issues', {
-                owner: 'WebOfTrust',
-                repo: 'WOT-terms',
-                title: issueData.title,
-                body: issueData.body,
-                // labels: [
-                //     'bug'
-                // ],
-                headers: {
-                    'X-GitHub-Api-Version': '2022-11-28'
-                }
-            });
+            // octokit.request('POST /repos/WebOfTrust/WOT-terms/issues', {
+            //     owner: 'WebOfTrust',
+            //     repo: 'WOT-terms',
+            //     title: issueData.title,
+            //     body: issueData.body,
+            //     // labels: [
+            //     //     'bug'
+            //     // ],
+            //     headers: {
+            //         'X-GitHub-Api-Version': '2022-11-28'
+            //     }
+            // });
 
-            console.log('GitHub issue created.');
+            // console.log('GitHub issue created.');
         });
     }
 });
