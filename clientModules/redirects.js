414423368fe77dc38f51a51ea6603be93e7aa1ef
kor <kor@dwarshuis.com>
2024-02-26 21:55:24 +0100

    fix: test if (typeof window !== 'undefined')

--- clientModules/redirects.js
+++ clientModules/redirects.js
@@ -1,3 +1,7 @@
-if (window.location.href.includes("https://weboftrust.github.io/WOT-terms/json/external-glosseries/glossaries-combined/all-glossaries.json")) {
-  window.location.href = "https://weboftrust.github.io/WOT-terms/json/external-glosseries/glossaries-combined/dictionary.json";
+if (typeof window !== 'undefined') {
+  // Code to run in the client-side environment (browser)
+  if (window.location.href.includes("https://weboftrust.github.io/WOT-terms/json/external-glosseries/glossaries-combined/all-glossaries.json")) {
+    window.location.href = "https://weboftrust.github.io/WOT-terms/json/external-glosseries/glossaries-combined/dictionary.json";
+  }
+
 }
