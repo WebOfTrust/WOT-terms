07023185107fe6e73ca20d3ef83f02a2dfea0f55
kor <kor@dwarshuis.com>
2024-04-23 20:27:06 +0200

    feat: Disable button if no more issues to show

--- src/components/issues/PaginationButtons.js
+++ src/components/issues/PaginationButtons.js
@@ -1,20 +1,18 @@
 // PaginationButtons.js
 import React from 'react';
 
-const PaginationButtons = ({ counter, setCounter, fetchIssues }) => (
+const PaginationButtons = ({ counter, setCounter, fetchIssues, issuesLength }) => (
     <div className="d-flex justify-content-center mb-5">
         <div className="btn-group">
-            <a className="btn btn-outline-secondary" onClick={() => {
+            <a className={`btn btn-outline-secondary ${issuesLength === 0 ? 'disabled' : ''}`} onClick={() => {
                 setCounter(prevCounter => prevCounter + 1);
                 fetchIssues(counter + 1);
-                console.log('counter: ', counter + 1);
             }}>Older</a>
 
             <a className={`btn btn-outline-secondary ${counter === 1 ? 'disabled' : ''}`} onClick={() => {
                 if (counter > 1) { // Make sure counter is not less than 1
                     setCounter(prevCounter => prevCounter - 1);
                     fetchIssues(counter - 1);
-                    console.log('counter: ', counter - 1);
                 }
             }}>Newer</a>
         </div>
