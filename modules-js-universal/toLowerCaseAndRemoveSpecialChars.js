a9f44e11f0aaf8db75aa6032ccd5c72dc8f28d4c
kor <kor@dwarshuis.com>
2024-05-06 18:43:57 +0200

    feat: Add a well written JSDoc block at file level + Module JSDoc block

--- modules-js-universal/toLowerCaseAndRemoveSpecialChars.js
+++ modules-js-universal/toLowerCaseAndRemoveSpecialChars.js
@@ -1,7 +1,14 @@
 /**
+ * @file This file converts string to lower case and removes special characters
+ * @author Kor Dwarshuis
+ * @version 1.0.0
+ * @since 2023-12-01
+ */
+
+/**
  * Description: Converts string to lower case and removes special characters
  * @param {*} str 
- * @returns 
+ * @returns {string} - The string in lower case without special characters
  */
 export function toLowerCaseAndRemoveSpecialChars(str) {
     return str.toLowerCase().replace(/[\s-_—]/g, '');
