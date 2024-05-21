45bd10a224cbb87c13ff9da5e125eb38b5db1238
kor <kor@dwarshuis.com>
2024-05-21 20:52:58 +0200

    feat: Add call to isLibraryInstalled.mjs plus more console.logs

--- search-index-typesense/scrape_start.sh
+++ search-index-typesense/scrape_start.sh
@@ -9,8 +9,9 @@ source ./${SEARCH_INDEX_DIR}/logger.sh
 #########################
 # TEST IF REQUIRED LIBRARIES ARE INSTALLED
 #########################
-node "$SCRIPT_DIR/isLibraryInstalled.mjs"
 setLogFile "success.log"
+log "Start testing libraries is finished"
+node "$SCRIPT_DIR/isLibraryInstalled.mjs"
 log "Testing libraries is finished (check outcome in success.log and error.log)"
 
 
@@ -29,62 +30,73 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #########################
 
 # Prepare file system.
-source "$SCRIPT_DIR/prepare_file_system.sh"
 setLogFile "success.log"
+log "Start preparing file system"
+source "$SCRIPT_DIR/prepare_file_system.sh"
 log "Preparing file system finished"
 
 # Copy handmade stuff.
-source "$SCRIPT_DIR/copy_manual_files.sh"
 setLogFile "success.log"
+log "Start copying manual files"
+source "$SCRIPT_DIR/copy_manual_files.sh"
 log "Copying manual files finished"
 
 # Create sitemaps.
-source "$SCRIPT_DIR/config/config_sitemaps_create.sh"
 setLogFile "success.log"
+log "Start creating sitemaps"
+source "$SCRIPT_DIR/config/config_sitemaps_create.sh"
 log "Creating sitemaps finished"
 
 # Remove unwanted urls from the sitemaps (new sitemaps generated or not)
-node "$SCRIPT_DIR/removeURLsFromSitemap.mjs"
 setLogFile "success.log"
+log "Start extracting data"
+node "$SCRIPT_DIR/removeURLsFromSitemap.mjs"
 log "Extracting data finished"
 
 
 # Filenames to lowercase.
-node "${SCRIPT_DIR}/renameFilesToLowerCase.mjs" ${SEARCH_INDEX_DIR}/sitemaps
 setLogFile "success.log"
+log "Start renaming files to lowercase"
+node "${SCRIPT_DIR}/renameFilesToLowerCase.mjs" ${SEARCH_INDEX_DIR}/sitemaps
 log "Renaming files to lowercase finished"
 
 # Fetch external content.
-node fetchExternalContent/fetchSingleUrlsFromWotTermsGoogleSheet/fetchSingleUrlsFromWotTermsGoogleSheet.js
 setLogFile "success.log"
-log "Fetch external content finished"
+log "Start fetching external content"
+node fetchExternalContent/fetchSingleUrlsFromWotTermsGoogleSheet/fetchSingleUrlsFromWotTermsGoogleSheet.js
+log "Fetching external content finished"
 
 #########################
 # START SCRAPING
 #########################
 
 # Scrape the websites.
-node "$SCRIPT_DIR/extractData.mjs"
 setLogFile "success.log"
+log "Start extracting data"
+node "$SCRIPT_DIR/extractData.mjs"
 log "Extracting data finished"
 
 # Split the content.jsonl file into multiple files so the size is optimal for Typesense.
-node "$SCRIPT_DIR/splitContentJSONL.mjs"
 setLogFile "success.log"
+log "Start splitting content"
+node "$SCRIPT_DIR/splitContentJSONL.mjs"
 log "Splitting content finished"
 
 # Count the total number of lines in all .jsonl files and write it to log dir.
-node "$SCRIPT_DIR/countLinesInJsonlFiles.mjs"
 setLogFile "success.log"
+log "Start counting number of lines"
+node "$SCRIPT_DIR/countLinesInJsonlFiles.mjs"
 log "Counting number of lines finished"
 
-node "$SCRIPT_DIR/collectScrapedUrls.mjs" "${SEARCH_INDEX_DIR}/${SEARCH_INDEX_ENTRIES_DIR}" "${INDEX_OVERVIEW_FILE}"
 setLogFile "success.log"
+log "Start collecting urls and writing to index file"
+node "$SCRIPT_DIR/collectScrapedUrls.mjs" "${SEARCH_INDEX_DIR}/${SEARCH_INDEX_ENTRIES_DIR}" "${INDEX_OVERVIEW_FILE}"
 log "Collecting urls and writing to index file finished"
 
 # Sort and style the index file.
-node "$SCRIPT_DIR/sortAndStyleScrapedIndex.mjs" "$INDEX_OVERVIEW_FILE"
 setLogFile "success.log"
+log "Start sorting and styling index file"
+node "$SCRIPT_DIR/sortAndStyleScrapedIndex.mjs" "$INDEX_OVERVIEW_FILE"
 log "Sorting and styling index file finished"
 
 
@@ -94,6 +106,7 @@ log "Sorting and styling index file finished"
 #########################
 
 # Backup output (scrape results, handmade stuff, sitemaps, logs, webpage overview, typesense export).
-source "$SCRIPT_DIR/backup.sh"
 setLogFile "success.log"
-log "Backup finished"
+log "Start creating backup"
+source "$SCRIPT_DIR/backup.sh"
+log "Creating backup finished"
