70561196cdc0ebd0cbfd96da86339d1bc8e4dc36
kor <kor@dwarshuis.com>
2024-07-12 21:57:47 +0200

    Bring HomepageFeatures back

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
     >
       <HomepageHeader />
       <main>
-        {/* <HomepageFeatures /> */}
+        <HomepageFeatures />
       </main>
     </Layout>
   );
