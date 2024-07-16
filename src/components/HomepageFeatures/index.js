44690168e74593699cdbc325bc4c37a2729f8040
kor <kor@dwarshuis.com>
2024-07-16 09:23:03 +0200

    Make blocks clickable

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -7,39 +7,44 @@ const FeatureList = [
     title: '⚡ KERIDoc',
     Svg: require('@site/static/img/10119973341678815049.svg').default,
     description: (
-      <>
-        <a href="https://weboftrust.github.io/keridoc/">KERIDoc</a>
-      </>
+      <></>
     ),
+    url: 'https://weboftrust.github.io/keridoc/'
   },
   {
     title: '⚡ KERI Suite Glossary',
     Svg: require('@site/static/img/9491177161682829258.svg')
       .default,
     description: (
-      <><a href="https://weboftrust.github.io/WOT-terms/">KERI Suite Glossary</a></>),
+      <></>),
+    url: 'https://weboftrust.github.io/WOT-terms/'
   },
   {
     title: '⚡ Kerisse',
     Svg: require('@site/static/img/1626701221679047824.svg')
       .default,
     description: (
-      <><a href="https://weboftrust.github.io/kerisse/">KERISSE</a></>
+      <></>
     ),
+    url: 'https://weboftrust.github.io/kerisse/'
   },
 ];
 
-function Feature({ Svg, title, description }) {
+function Feature({ Svg, title, description, url }) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="margin-top--lg text--center">
-        <Svg className={styles.featureSvg} role="img" />
+    (
+      <div className={clsx('col col--4')}>
+        <a className='feature-block' href={`${url}`}>
+          <div className="margin-top--lg text--center">
+            <Svg className={styles.featureSvg} role="img" />
+          </div>
+          <div className="margin-top--lg text--center padding-horiz--md">
+            <h3>{title}</h3>
+            <p>{description}</p>
+          </div>
+        </a>
       </div>
-      <div className="margin-top--lg text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
+    )
   );
 }
 
