839d48288d02a2c1e9d58b6b1a033a85107fda44
kor <kor@dwarshuis.com>
2024-09-08 20:41:48 +0200

    Remove highlightSearchTermsOnTargetPage: true

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -255,8 +255,6 @@ const config = {
     [
       require.resolve('@cmfcmf/docusaurus-search-local'),
       {
-        highlightSearchTermsOnTargetPage: true,
-
         // whether to index docs pages
         indexDocs: true,
 
